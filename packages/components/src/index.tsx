2322d8dc794bb4157636801dfc87e9bdddc0d415
Antonette Caldwell <134739862+nebula-aac@users.noreply.github.com>
2023-08-23 17:44:51 +0000

    removed @mui/types, and added dialog, divider, and tooltip
    Signed-off-by: Antonette Caldwell <134739862+nebula-aac@users.noreply.github.com>

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -3,12 +3,15 @@ import { Avatar } from './avatar';
 import { BaseButton } from './button';
 import { Box } from './box';
 import { Chip } from './chip';
+import { Dialog } from './dialog';
+import { Divider } from './divider';
 import { IconButton } from './iconbutton';
 import { Paper } from './paper';
 import { Stack } from './stack';
 import { Tab } from './tab';
 import { Tabs } from './tabs';
 import { Toolbar } from './toolbar';
+import { Tooltip } from './tooltip';
 import { Typography } from './typography';
 
-export { AppBar, Avatar, BaseButton, Box, Chip, IconButton, Paper, Stack, Tab, Tabs, Toolbar, Typography };
+export { AppBar, Avatar, BaseButton, Box, Chip, Dialog, Divider, IconButton, Paper, Stack, Tab, Tabs, Toolbar, Tooltip, Typography };
