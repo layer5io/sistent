7fc5c0d39a2d409d1055beaabc3b913c9542815d
Antonette Caldwell <134739862+nebula-aac@users.noreply.github.com>
2023-08-23 19:21:44 +0000

    ignore dist in components, and added dialog components

---
+++ packages/components/src/dialogactions.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { DialogActions as MuiDialogActions, DialogActionsProps } from "@mui/material";
+
+export function DialogActions(props: DialogActionsProps) {
+    return <MuiDialogActions {...props} />
+}
---
+++ packages/components/src/dialogcontenttext.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { DialogContentText as MuiDialogContentText, DialogContentTextProps } from "@mui/material";
+
+export function DialogContentText(props: DialogContentTextProps) {
+    return <MuiDialogContentText {...props} />
+}
