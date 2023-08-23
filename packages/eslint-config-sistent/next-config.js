8c15368f32b33d1a4b39255b7bb6123e405c786c
Antonette Caldwell <acald.nebula@gmail.com>
2023-08-23 18:21:44 -0500

    (wip) eslint common pkg
    Signed-off-by: Antonette Caldwell <acald.nebula@gmail.com>

---
+++ packages/eslint-config-sistent/next-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ["next", "prettier"],
+    rules: {
+        "@next/next/no-html-link-for-pages": "off",
+    },
+    parserOptions: {
+        babelOptions: {
+            presets: [require.resolve("next/babel")],
+        },
+    },
+};
