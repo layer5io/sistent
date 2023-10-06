d5b64f23ce2d82f7e20dfdcc1a14a155669bbd3a
Antonette Caldwell <acald.nebula@gmail.com>
2023-10-05 20:55:10 -0500

    feat(components): added ClickAwayListener
    fix #123

---
+++ packages/components/src/base/ClickAwayListener/ClickAwayListener.tsx
@@ -0,0 +1,7 @@
+import {
+  ClickAwayListener as MuiClickAwayListener,
+  type ClickAwayListenerProps
+} from '@mui/material';
+export function ClickAwayListener(props: ClickAwayListenerProps) {
+  return <MuiClickAwayListener {...props} />;
+}
