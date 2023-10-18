18232a43038cae9638c2bcf7478835731f72f325
Antonette Caldwell <acald.nebula@gmail.com>
2023-10-18 17:16:56 -0500

    fix: resolve alias to sistent-components and sistent-svg library
    Signed-off-by: Antonette Caldwell <acald.nebula@gmail.com>

--- apps/design-system/vite.config.ts
+++ apps/design-system/vite.config.ts
@@ -1,7 +1,20 @@
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /@layer5\/sistent-components\/(.*)/,
+        replace: path.resolve(__dirname, 'node_modules', '@layer5', 'sistent-components')
+      },
+      {
+        find: /@layer5\/sistent-svg\/(.*)/,
+        replace: path.resolve(__dirname, 'node_modules', '@layer5', 'sistent-svg')
+      }
+    ]
+  }
 });
