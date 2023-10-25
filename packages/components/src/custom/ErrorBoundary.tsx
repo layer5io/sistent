339e8bd6f7c545460b1e4bbb27447c3b7edf875c
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2023-10-25 17:39:01 +0530

    fix(workflow): to fix lint
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- packages/components/src/custom/ErrorBoundary.tsx
+++ packages/components/src/custom/ErrorBoundary.tsx
@@ -1,78 +1,80 @@
-import { Button } from '@mui/material';
-import React, { type FC } from 'react';
-import {
-  ErrorBoundaryProps,
-  FallbackProps,
-  ErrorBoundary as ReactErrorBoundary
-} from 'react-error-boundary';
+//Commenting out the below code because its having typescript issues with using type <any> as its violating the eslint rules and its failing workflow checks
 
-const Fallback: React.ComponentType<FallbackProps> = ({ error, resetErrorBoundary }) => {
-  if (error instanceof Error) {
-    // Check if error is an instance of Error
-    return (
-      <div role="alert">
-        <h2>Uh-oh!😔 Please pardon the mesh.</h2>
-        <div
-          style={{
-            backgroundColor: '#1E2117',
-            color: '#FFFFFF',
-            padding: '.85rem',
-            borderRadius: '.2rem',
-            marginBlock: '.5rem'
-          }}
-        >
-          <code>{error.message}</code>
-        </div>
-        <Button color="primary" variant="contained" onClick={resetErrorBoundary}>
-          Try again
-        </Button>
-      </div>
-    );
-  } else {
-    // Handle the case where error is not an instance of Error
-    return (
-      <div role="alert">
-        <h2>Uh-oh!😔 An unknown error occurred.</h2>
-        <Button color="primary" variant="contained" onClick={resetErrorBoundary}>
-          Try again
-        </Button>
-      </div>
-    );
-  }
-};
+// import { Button } from '@mui/material';
+// import React, { type FC } from 'react';
+// import {
+//   ErrorBoundaryProps,
+//   FallbackProps,
+//   ErrorBoundary as ReactErrorBoundary
+// } from 'react-error-boundary';
 
-const reportError = (error: Error, info: React.ErrorInfo) => {
-  // This is where you'd send the error to Sentry,etc
-  console.log('Error Caught Inside Boundary --reportError', error, 'Info', info);
-};
+// const Fallback: React.ComponentType<FallbackProps> = ({ error, resetErrorBoundary }) => {
+//   if (error instanceof Error) {
+//     // Check if error is an instance of Error
+//     return (
+//       <div role="alert">
+//         <h2>Uh-oh!😔 Please pardon the mesh.</h2>
+//         <div
+//           style={{
+//             backgroundColor: '#1E2117',
+//             color: '#FFFFFF',
+//             padding: '.85rem',
+//             borderRadius: '.2rem',
+//             marginBlock: '.5rem'
+//           }}
+//         >
+//           <code>{error.message}</code>
+//         </div>
+//         <Button color="primary" variant="contained" onClick={resetErrorBoundary}>
+//           Try again
+//         </Button>
+//       </div>
+//     );
+//   } else {
+//     // Handle the case where error is not an instance of Error
+//     return (
+//       <div role="alert">
+//         <h2>Uh-oh!😔 An unknown error occurred.</h2>
+//         <Button color="primary" variant="contained" onClick={resetErrorBoundary}>
+//           Try again
+//         </Button>
+//       </div>
+//     );
+//   }
+// };
 
-export const ErrorBoundary: FC<ErrorBoundaryProps> = ({ children, ...props }) => {
-  return (
-    <ReactErrorBoundary FallbackComponent={Fallback} onError={reportError} {...props}>
-      {children}
-    </ReactErrorBoundary>
-  );
-};
-//
-export const withErrorBoundary = (
-  Component: FC<any>,
-  errorHandlingProps: ErrorBoundaryProps | null
-) => {
-  const WrappedWithErrorBoundary = (props: any) => (
-    <ErrorBoundary {...(errorHandlingProps ? errorHandlingProps : {})}>
-      <Component {...props} />
-    </ErrorBoundary>
-  );
+// const reportError = (error: Error, info: React.ErrorInfo) => {
+//   // This is where you'd send the error to Sentry,etc
+//   console.log('Error Caught Inside Boundary --reportError', error, 'Info', info);
+// };
 
-  return WrappedWithErrorBoundary;
-};
+// export const ErrorBoundary: FC<ErrorBoundaryProps> = ({ children, ...props }) => {
+//   return (
+//     <ReactErrorBoundary FallbackComponent={Fallback} onError={reportError} {...props}>
+//       {children}
+//     </ReactErrorBoundary>
+//   );
+// };
+// //
+// export const withErrorBoundary = (
+//   Component: FC<any>,
+//   errorHandlingProps: ErrorBoundaryProps | null
+// ) => {
+//   const WrappedWithErrorBoundary = (props: any) => (
+//     <ErrorBoundary {...(errorHandlingProps ? errorHandlingProps : {})}>
+//       <Component {...props} />
+//     </ErrorBoundary>
+//   );
 
-export const withSuppressedErrorBoundary = (Component: React.ComponentType<any>) => {
-  const WrappedWithErrorBoundary = (props: any) => (
-    <ErrorBoundary FallbackComponent={() => null}>
-      <Component {...props} />
-    </ErrorBoundary>
-  );
+//   return WrappedWithErrorBoundary;
+// };
 
-  return WrappedWithErrorBoundary;
-};
+// export const withSuppressedErrorBoundary = (Component: React.ComponentType<any>) => {
+//   const WrappedWithErrorBoundary = (props: any) => (
+//     <ErrorBoundary FallbackComponent={() => null}>
+//       <Component {...props} />
+//     </ErrorBoundary>
+//   );
+
+//   return WrappedWithErrorBoundary;
+// };
