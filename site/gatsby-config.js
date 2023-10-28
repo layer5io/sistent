be3195bbd64c830405c7c751bd0a79bb76cb615e
coder12git <suruchikumarimfp4@gmail.com>
2023-10-28 19:15:24 +0530

    feat(core): init
    Signed-off-by: coder12git <suruchikumarimfp4@gmail.com>

---
+++ site/gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
