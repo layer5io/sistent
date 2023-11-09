765d194b8aadbde019b93b106dfd2ed088fbca3a
Antonette Caldwell <acald.nebula@gmail.com>
2023-11-08 18:55:23 -0600

    fix(components): remove custom-table.tsx
    Signed-off-by: Antonette Caldwell <acald.nebula@gmail.com>

--- packages/components/src/custom/Toolbar/index.tsx
+++ packages/components/src/custom/Toolbar/index.tsx
@@ -1,6 +1,5 @@
 import CustomColumnVisibilityControl from './custom-column';
 import UniversalFilter from './custom-filter';
 import SearchBar from './custom-search';
-import ResponsiveDataTable from './custom-table';
 
-export { CustomColumnVisibilityControl, ResponsiveDataTable, SearchBar, UniversalFilter };
+export { CustomColumnVisibilityControl, SearchBar, UniversalFilter };
