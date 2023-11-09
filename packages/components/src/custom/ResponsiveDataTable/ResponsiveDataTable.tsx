6e80b753bb31be88c98abad1c3f28b8a607ea016
Antonette Caldwell <acald.nebula@gmail.com>
2023-11-08 18:35:40 -0600

    fix(components): fix data table exports
    Signed-off-by: Antonette Caldwell <acald.nebula@gmail.com>

---
+++ packages/components/src/custom/ResponsiveDataTable/ResponsiveDataTable.tsx
@@ -0,0 +1,124 @@
+import MUIDataTable from 'mui-datatables';
+import { useEffect, useState } from 'react';
+
+interface Column {
+  name: string;
+  label: string;
+  options?: {
+    filter?: boolean;
+    sort?: boolean;
+    searchable?: boolean;
+    display?: boolean;
+    sortDescFirst?: boolean;
+    customBodyRender?: (value: string | number | boolean | object) => JSX.Element;
+  };
+}
+
+interface ResponsiveDataTableProps {
+  data: string[][];
+  columns: Column[];
+  options?: object;
+  tableCols?: Column[];
+  updateCols?: ((columns: Column[]) => void) | undefined;
+  columnVisibility: Record<string, boolean> | undefined;
+  theme?: object;
+}
+
+function ResponsiveDataTable({
+  data,
+  columns,
+  options = {},
+  ...props
+}: ResponsiveDataTableProps): JSX.Element {
+  const [tableCols, updateCols] = useState(columns);
+  const [columnVisibility, ,] = useState<Record<string, boolean>>({});
+
+  const formatDate = (date: Date): string => {
+    const dateOptions: Intl.DateTimeFormatOptions = {
+      weekday: 'short',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    };
+
+    return new Intl.DateTimeFormat('un-US', dateOptions).format(date);
+  };
+
+  const updatedOptions = {
+    ...options,
+    onViewColumnsChange: (column: string, action: string) => {
+      switch (action) {
+        case 'add': {
+          const colToAdd = columns.find((obj) => obj.name === column);
+          if (colToAdd) {
+            colToAdd.options!.display = true;
+            updateCols([...columns]);
+          }
+          break;
+        }
+        case 'remove': {
+          const colToRemove = columns.find((obj) => obj.name === column);
+          if (colToRemove) {
+            colToRemove.options!.display = false;
+            updateCols([...columns]);
+          }
+          break;
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    columns?.forEach((col) => {
+      if (!col.options) {
+        col.options = {};
+      }
+      col.options.display = columnVisibility[col.name];
+
+      if (
+        ['updated_at', 'created_at', 'deleted_at', 'last_login_time', 'joined_at'].includes(
+          col.name
+        )
+      ) {
+        col.options.customBodyRender = (value: string | number | boolean | object) => {
+          if (value === 'NA') {
+            return <>{value}</>;
+          } else if (typeof value === 'object' && 'Valid' in value) {
+            const obj = value as { Valid: boolean; Time: string | undefined };
+            if (obj.Valid && obj.Time) {
+              const date = new Date(obj.Time);
+              return <>{formatDate(date)}</>;
+            } else {
+              return <>NA</>;
+            }
+          } else if (typeof value === 'string') {
+            const date = new Date(value);
+            return <>{formatDate(date)}</>;
+          } else {
+            return <>{value}</>;
+          }
+        };
+      }
+    });
+    updateCols([...columns]);
+  }, [columnVisibility]);
+
+  const components = {
+    ExpandButton: () => ''
+  };
+
+  return (
+    <MUIDataTable
+      columns={columns || []}
+      data={data || []}
+      title={undefined}
+      components={components}
+      options={updatedOptions}
+      tableCols={tableCols}
+      updateCols={updateCols}
+      {...props}
+    />
+  );
+}
+
+export default ResponsiveDataTable;
---
+++ packages/components/src/custom/ResponsiveDataTable/index.tsx
@@ -0,0 +1,1 @@
+export { ResponsiveDataTable } from './ResponsiveDataTable';
