9cff8bbb27f31b36660a5aa8eb872a8e3b917beb
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2023-11-12 00:41:22 +0530

    fix(viewbox): use correct viewbox
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- packages/svg/src/icons/Filter/FilterIcon.tsx
+++ packages/svg/src/icons/Filter/FilterIcon.tsx
@@ -12,7 +12,7 @@ const FilterIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
       width={width}
       height={height}
       fill={fill}
-      viewBox="0 0 100 100"
+      viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
