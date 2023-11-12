230b59724d82ba186772a678a2f14306686e3397
Antonette Caldwell <acald.nebula@gmail.com>
2023-11-11 20:46:11 -0600

    refactor(svg): split up Add icons, and rename AddIconCircleBordered to AddCircleIcon
    BREAKING CHANGE: Renamed AddIconCircleBordered to AddCircleIcon. Also configured both AddIcon and
    AddCircleIcon to use the new IconProps
    
    fix #322

--- packages/svg/src/icons/Add/AddIcon.tsx
+++ packages/svg/src/icons/Add/AddIcon.tsx
@@ -1,13 +1,12 @@
-import { FC } from 'react';
 import { DEFAULT_FILL_NONE, DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../constants/constants';
 import { IconProps } from '../types';
 
-export const AddIcon: FC<IconProps> = ({
+export const AddIcon = ({
   width = DEFAULT_WIDTH,
   height = DEFAULT_HEIGHT,
   fill = DEFAULT_FILL_NONE,
   ...props
-}) => {
+}: IconProps): JSX.Element => {
   return (
     <svg
       width={width}
