d1f95313406b19ab0f43910f1ca918a25baf3961
Senali <senalidilumika@gmail.com>
2023-12-16 22:17:39 +0530

    feat: formatting
    Signed-off-by: Senali <senalidilumika@gmail.com>

--- packages/components/src/custom/index.tsx
+++ packages/components/src/custom/index.tsx
@@ -1,9 +1,9 @@
 // import { ErrorBoundary, withErrorBoundary, withSuppressedErrorBoundary } from './ErrorBoundary';
+import { ConnectionChip } from './ConnectionChip';
 import { FlipCard } from './FlipCard';
 import { useWindowDimensions } from './Helpers/Dimension';
 import { useNotificationHandler } from './Helpers/Notification';
 import { StyledTooltip } from './Tooltip';
-import { ConnectionChip } from './ConnectionChip';
 
 export { StyledChartDialog } from './ChartDialog';
 export { StyledSearchBar } from './StyledSearchBar';
