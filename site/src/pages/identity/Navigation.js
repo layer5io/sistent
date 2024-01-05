2999cf72b812a17e1933cb3e978910f14b6554e2
Lakshya Satpal <lakshyasatpal372@gmail.com>
2024-01-06 04:12:13 +0530

    fix: import React in 404.js
    Signed-off-by: Lakshya Satpal <lakshyasatpal372@gmail.com>

--- site/src/pages/identity/Navigation.js
+++ site/src/pages/identity/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navigation = (props) => {
   console.log('naviation');
