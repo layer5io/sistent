268d7dba6388b40694d0f73df031e71373f809ee
yash sharma <yashsharma2572@gmail.com>
2024-01-23 20:32:25 +0530

    feat(): Added help and support modal rjsf schema
    Signed-off-by: yash sharma <yashsharma2572@gmail.com>

---
+++ packages/components/src/schemas/supportModal/rjsfSchema.tsx
@@ -0,0 +1,38 @@
+/**
+ * Represents the JSON Schema for the Help and Support modal form.
+ * This schema is designed to capture information for support request.
+ *
+ * Playground link -
+ */
+const helpAndSupportModalSchema = {
+  title: 'Support Form',
+  properties: {
+    subject: {
+      type: 'string',
+      title: 'Subject',
+      description:
+        'Enter a concise and descriptive title for your support request. This will help us quickly understand the nature of your inquiry.',
+      minLength: 1,
+      'x-rjsf-grid-area': '12'
+    },
+    description: {
+      type: 'string',
+      title: 'Description',
+      description:
+        'Please provide a detailed description of your issue or question. Include any relevant information that you think will help us assist you more effectively. The more details you provide, the better we can understand and address your concerns.',
+      minLength: 10,
+      format: 'textarea',
+      'x-rjsf-grid-area': '12'
+    },
+    scope: {
+      type: 'string',
+      enum: ['Support', 'Community', 'Account', 'Commercial'],
+      title: 'Scope of Questions',
+      description: 'Select the category that best represents the nature of your inquiry.',
+      default: 'Technical'
+    }
+  },
+  required: ['subject', 'description']
+};
+
+export default helpAndSupportModalSchema;
