af80431d19f376f7412639380f9ce95862b4c8d2
Antonette Caldwell <acald.nebula@gmail.com>
2024-02-08 10:09:34 -0600

    chore: add and remove packages
    Signed-off-by: Antonette Caldwell <acald.nebula@gmail.com>

--- examples/next-14/next.config.mjs
+++ examples/next-14/next.config.mjs
@@ -3,6 +3,44 @@ import million from 'million/compiler';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    relay: {
+      // ...
+      // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
+      src: './components',
+      schema: './schema.graphql',
+      language: 'javascript',
+      excludes: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
+    },
+  },
+  async rewrites() {
+    return [
+      { source: '/api/:path*', destination: 'http://localhost:9081/:path*' },
+      { source: '/user/:path*', destination: 'http://localhost:9081/:path*' },
+      { source: '/provider/:path*', destination: '/:path*' },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const millionConfig = {
