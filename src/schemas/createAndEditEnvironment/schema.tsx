38d83d1a2399a6c135425994fe6d448c535f1ea3
yash sharma <yashsharma2572@gmail.com>
2024-02-13 03:44:49 +0530

    feat(): export schema properly
    Signed-off-by: yash sharma <yashsharma2572@gmail.com>

--- src/schemas/createAndEditEnvironment/schema.tsx
+++ src/schemas/createAndEditEnvironment/schema.tsx
@@ -2,7 +2,8 @@ const createAndEditEnvironmentSchema = {
   title: 'Environment',
   properties: {
     description: {
-      description: 'Description of the Environment',
+      description:
+        'An environment is a collection of resources, such as connections & credentail. Provide a detailed description to clarify the purpose of this environment and the types of resources it encompasses. You can modify the description at any time. Learn more about environments [here](https://docs.meshery.io/concepts/logical/environments).',
       format: 'textarea',
       title: 'Description',
       type: 'string',
@@ -10,7 +11,7 @@ const createAndEditEnvironmentSchema = {
     },
     name: {
       description:
-        "Enter a unique and meaningful name for the environment. Remember you can change name of environment after it's creation too",
+        'An environment is a collection of resources. Provide a name that meaningfully represents these resources. You can change the name of the environment even after its creation.',
       title: 'Name',
       type: 'string',
       'x-rjsf-grid-area': '12'
@@ -18,7 +19,8 @@ const createAndEditEnvironmentSchema = {
     organization: {
       type: 'string',
       title: 'Organization',
-      description: 'Select a organization in whic',
+      description:
+        'Select an organization in which you want to create this new environment. Keep in mind that the organization cannot be changed after creation.',
       enum: [],
       enumNames: [],
       'x-rjsf-grid-area': '12'
--- src/schemas/createAndEditWorkspace/schema.tsx
+++ src/schemas/createAndEditWorkspace/schema.tsx
@@ -4,20 +4,24 @@
 const createAndEditWorkspace = {
   properties: {
     description: {
-      description: 'Description of the Workspace',
+      description:
+        "Workspaces serve as a virtual space for your team-based work, allows you to control access and more, Provide a detailed description to clarify the purpose of this workspace. Remember you can changes description of workspace after it's creations too. Learn more about workspaces [here](https://docs.meshery.io/concepts/logical/workspaces)",
       format: 'textarea',
       title: 'Description',
       type: 'string',
       'x-rjsf-grid-area': '12'
     },
     name: {
-      description: 'The name of Workspace',
+      description:
+        'Provide a name that meaningfully represents this workspace. You can change the name of the workspace even after its creation.',
       title: 'Name',
       type: 'string',
       'x-rjsf-grid-area': '12'
     },
     organization: {
       type: 'string',
+      description:
+        'Select an organization in which you want to create this new workspace. Keep in mind that the organization cannot be changed after creation.',
       title: 'Organization',
       enum: [],
       enumNames: [],
