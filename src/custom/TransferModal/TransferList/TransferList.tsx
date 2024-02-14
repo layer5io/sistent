30e33094408dea7975bfff462d5ec4baa43a7662
sudhanshu dasgupta <dasguptashivam23@gmail.com>
2024-02-14 18:08:57 +0530

    initial transferlist component
    Signed-off-by: sudhanshu dasgupta <dasguptashivam23@gmail.com>

---
+++ src/custom/TransferModal/TransferList/TransferList.tsx
@@ -0,0 +1,243 @@
+import React from 'react';
+import { Checkbox, Grid, List, ListItem, Typography } from '../../../base';
+import { KubernetesIcon, LeftArrowIcon, RightArrowIcon, SMPIcon } from '../../../icons';
+import Tooltip from '../../../patches/Tooltip';
+import {
+  ButtonGrid,
+  ListGrid,
+  ListHeading,
+  StyledChip,
+  StyledPaper,
+  TransferButton
+} from './style';
+
+export const TRANSFER_COMPONET = {
+  CHIP: 'chip',
+  OTHER: 'other'
+};
+
+function not(a: any, b: any) {
+  return a.filter((value: any) => b.indexOf(value) === -1);
+}
+
+function intersection(a: any, b: any) {
+  return a.filter((value: any) => b.indexOf(value) !== -1);
+}
+
+/**
+ * Renders transfer component.
+ *
+ * @param {Object} props - The component props.
+ * @param {String} props.name - This is the name of the data list.
+ * @param {Array} props.assignableData - The assignable data list.
+ * @param {Function} props.assignedData - The callback function to transfer assigned data list.
+ * @param {Array} props.originalAssignedData - The already assigend data list.
+ * @param {Element} props.emptyStateIconLeft - Icon for empty state of list left.
+ * @param {String} props.emtyStateMessageLeft - Message for the empty state of the list left.
+ * @param {Element} props.emptyStateIconRight - Icon for empty state of list right.
+ * @param {String} props.emtyStateMessageRight - Message for the empty state of the list right.
+ * @param {String} props.transferComponentType - Type of the component transfer (There is two types: chip and other).
+ */
+
+export interface TransferListProps {
+  name: string;
+  assignableData: Array<any>;
+  assignedData: Function;
+  originalAssignedData: Array<any>;
+  emptyStateIconLeft: JSX.Element;
+  emtyStateMessageLeft: string;
+  emptyStateIconRight: JSX.Element;
+  emtyStateMessageRight: string;
+  transferComponentType: string;
+}
+
+function TransferList({
+  name,
+  assignableData,
+  assignedData,
+  originalAssignedData,
+  emptyStateIconLeft,
+  emtyStateMessageLeft,
+  emptyStateIconRight,
+  emtyStateMessageRight,
+  transferComponentType = TRANSFER_COMPONET.OTHER
+}: TransferListProps): JSX.Element {
+  const [checked, setChecked] = React.useState<any[]>([]);
+  const [left, setLeft] = React.useState<any[]>([]);
+  const [right, setRight] = React.useState<any>(originalAssignedData);
+
+  const leftChecked = intersection(checked, left);
+  const rightChecked = intersection(checked, right);
+
+  React.useEffect(() => {
+    assignedData(right);
+    const idsToRemove = new Set(right.map((item: { id: number }) => item.id));
+    const filteredLeft = assignableData.filter((item) => !idsToRemove.has(item.id));
+    setLeft(filteredLeft);
+  }, [right, assignableData]);
+
+  const handleToggle = (value: any) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  const handleAllRight = () => {
+    setRight(right.concat(left));
+    setLeft([]);
+  };
+
+  const handleCheckedRight = () => {
+    setRight(right.concat(leftChecked));
+    setLeft(not(left, leftChecked));
+    setChecked(not(checked, leftChecked));
+  };
+
+  const handleCheckedLeft = () => {
+    setLeft(left.concat(rightChecked));
+    setRight(not(right, rightChecked));
+    setChecked(not(checked, rightChecked));
+  };
+
+  const handleAllLeft = () => {
+    setLeft(left.concat(right));
+    setRight([]);
+  };
+
+  const customList = (items: any[], emptyStateIcon: any, emtyStateMessage: string) => (
+    <StyledPaper>
+      <List dense component="div" role="list">
+        {items.length > 0 ? (
+          items.map((item) => {
+            const labelId = `transfer-list-item-${item.name}-label`;
+            return (
+              <ListItem
+                key={item.id}
+                role="listitem"
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  '&:hover': {
+                    backgroundColor: `#00000010`
+                  }
+                }}
+                onClick={handleToggle(item)}
+              >
+                {transferComponentType === TRANSFER_COMPONET.CHIP ? (
+                  <Tooltip title={item.name} placement="top">
+                    <StyledChip
+                      sx={{ paddingY: '10px' }}
+                      variant="outlined"
+                      label={item.name}
+                      onDelete={() => {}}
+                      deleteIcon={<SMPIcon />}
+                      icon={<KubernetesIcon />}
+                    />
+                  </Tooltip>
+                ) : (
+                  <Tooltip title={item.name} placement="top">
+                    <Typography sx={{ maxWidth: '230px', height: '1.5rem', overflow: 'hidden' }}>
+                      {item.name}
+                    </Typography>
+                  </Tooltip>
+                )}
+                <Checkbox
+                  checked={checked.indexOf(item) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{
+                    'aria-labelledby': labelId
+                  }}
+                />
+              </ListItem>
+            );
+          })
+        ) : (
+          <div
+            style={{
+              textAlign: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '264px'
+            }}
+          >
+            {emptyStateIcon}
+            <Typography sx={{ color: '#979797', px: 5, py: 2, lineHeight: 1 }}>
+              {emtyStateMessage}
+            </Typography>
+          </div>
+        )}
+      </List>
+    </StyledPaper>
+  );
+
+  return (
+    <Grid container justifyContent="center" alignItems="center">
+      <ListGrid>
+        <ListHeading>
+          Available {name} ({left.length})
+        </ListHeading>
+        {customList(left, emptyStateIconLeft, emtyStateMessageLeft)}
+      </ListGrid>
+      <ButtonGrid>
+        <Grid container direction="column" alignItems="center">
+          <TransferButton
+            variant="outlined"
+            size="small"
+            onClick={handleAllRight}
+            disabled={left.length === 0}
+            aria-label="move all right"
+          >
+            <RightArrowIcon width={18} height={18} />
+            <RightArrowIcon style={{ position: 'absolute', left: '27px' }} width={18} height={18} />
+          </TransferButton>
+          <TransferButton
+            variant="outlined"
+            size="small"
+            onClick={handleCheckedRight}
+            disabled={leftChecked.length === 0}
+            aria-label="move selected right"
+          >
+            <RightArrowIcon width={18} height={18} />
+          </TransferButton>
+          <TransferButton
+            variant="outlined"
+            size="small"
+            onClick={handleCheckedLeft}
+            disabled={rightChecked.length === 0}
+            aria-label="move selected left"
+          >
+            <LeftArrowIcon width={18} height={18} />
+          </TransferButton>
+          <TransferButton
+            variant="outlined"
+            size="small"
+            onClick={handleAllLeft}
+            disabled={right.length === 0}
+            aria-label="move all left"
+          >
+            <LeftArrowIcon width={18} height={18} />
+            <LeftArrowIcon style={{ position: 'absolute', left: '27px' }} width={18} height={18} />
+          </TransferButton>
+        </Grid>
+      </ButtonGrid>
+      <ListGrid>
+        <ListHeading>
+          Assigned {name} ({right.length})
+        </ListHeading>
+        {customList(right, emptyStateIconRight, emtyStateMessageRight)}
+      </ListGrid>
+    </Grid>
+  );
+}
+
+export default TransferList;
---
+++ src/custom/TransferModal/TransferList/style.tsx
@@ -0,0 +1,75 @@
+import { styled } from '@mui/material';
+import { Button, Chip, Grid, Paper, Typography } from '../../../base';
+import { ALICE_BLUE, BLACK, DARK_TEAL, KEPPEL, WHITE, accentGrey } from '../../../theme';
+import { STEEL_GRAY } from '../../../theme/colors/colors';
+
+export const StyledChip = styled(Chip)({
+  padding: '5px 6px !important',
+  color: BLACK,
+  fontSize: '14px',
+  textTransform: 'uppercase',
+  fontWeight: 400,
+  height: 'unset',
+  borderRadius: '100px',
+  border: `0.5px solid ${accentGrey[40]}`,
+  background: WHITE,
+  maxWidth: '230px'
+});
+
+export const StyledPaper = styled(Paper)({
+  width: 300,
+  height: 280,
+  overflow: 'auto',
+  backgroundColor: ALICE_BLUE,
+  borderRadius: '10px',
+  boxShadow: '0px 1px 4px 0px rgba(0, 0, 0, 0.25) inset',
+  '@media (max-width: 843px)': {
+    width: 260
+  },
+  '@media (max-width: 768px)': {
+    width: 300
+  },
+  '@media (max-width: 375px)': {
+    width: '100%'
+  }
+});
+
+export const ListHeading = styled(Typography)({
+  paddingBottom: '15px',
+  color: STEEL_GRAY,
+  textAlign: 'center',
+  fontSize: '1rem',
+  letterSpacing: '0.15px'
+});
+
+export const TransferButton = styled(Button)({
+  margin: '5px 0',
+  padding: '7px 0',
+  borderRadius: '10px',
+  borderColor: DARK_TEAL,
+  boxShadow: 'none',
+  '&:hover': {
+    borderColor: KEPPEL
+  }
+});
+
+export const ListGrid = styled(Grid)({
+  padding: '0 1rem',
+  '@media (max-width: 768px)': {
+    padding: '0',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    width: '100%'
+  }
+});
+
+export const ButtonGrid = styled(Grid)({
+  padding: '40px 1rem 0 1rem',
+  '@media (max-width: 768px)': {
+    padding: '1rem',
+    transform: 'rotate(90deg)',
+    height: '100px',
+    marginLeft: '100px'
+  }
+});
