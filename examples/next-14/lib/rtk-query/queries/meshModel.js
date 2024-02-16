b6128e7173011f917fcd7b9b4846508e4215d11f
Antonette Caldwell <acald.nebula@gmail.com>
2024-02-16 12:13:52 -0600

    feat: copy over rtk queries
    Signed-off-by: Antonette Caldwell <acald.nebula@gmail.com>

---
+++ examples/next-14/lib/rtk-query/queries/meshModel.js
@@ -0,0 +1,91 @@
+import { api } from '../config';
+
+import _ from 'lodash';
+
+const TAGS = {
+  MESH_MODELS: 'mesh-models',
+};
+
+const defaultOptions = {
+  trim: false,
+  // annotations: false,
+  search: '',
+  page: 1,
+  pagesize: 'all',
+};
+
+const meshModelApi = api
+  .enhanceEndpoints({
+    addTagTypes: [TAGS.MESH_MODELS],
+  })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      getMeshModels: builder.query({
+        query: (queryArgs) => ({
+          url: `meshmodels/models`,
+          params: _.merge({}, defaultOptions, queryArgs.params),
+        }),
+        providesTags: () => [{ type: TAGS.MESH_MODELS }],
+      }),
+      getComponents: builder.query({
+        query: (queryArgs) => ({
+          url: `meshmodels/components`,
+          params: _.merge({}, defaultOptions, queryArgs.params),
+        }),
+        providesTags: () => [{ type: TAGS.MESH_MODELS }],
+      }),
+      getRelationships: builder.query({
+        query: (queryArgs) => ({
+          url: `meshmodels/relationships`,
+          params: _.merge({}, defaultOptions, queryArgs.params),
+        }),
+        providesTags: () => [{ type: TAGS.MESH_MODELS }],
+      }),
+      getRegistrants: builder.query({
+        query: (queryArgs) => ({
+          url: `meshmodels/registrants`,
+          params: _.merge({}, defaultOptions, queryArgs.params),
+        }),
+        providesTags: () => [{ type: TAGS.MESH_MODELS }],
+      }),
+      getComponentsFromModal: builder.query({
+        query: (queryArgs) => ({
+          url: `meshmodels/models/${queryArgs.model}/components`,
+          params: _.merge({}, defaultOptions, queryArgs.params),
+        }),
+        providesTags: () => [{ type: TAGS.MESH_MODELS }],
+      }),
+      getRelationshipsFromModal: builder.query({
+        query: (queryArgs) => ({
+          url: `meshmodels/models/${queryArgs.model}/relationships`,
+          params: _.merge({}, defaultOptions, queryArgs.params),
+        }),
+        providesTags: () => [{ type: TAGS.MESH_MODELS }],
+      }),
+      getModelCategories: builder.query({
+        query: () => ({
+          url: `meshmodels/categories`,
+          method: 'GET',
+        }),
+        providesTags: () => [{ type: TAGS.MESH_MODELS }],
+      }),
+      getModelFromCategory: builder.query({
+        query: (queryArgs) => ({
+          url: `meshmodels/categories/${queryArgs.category}/models`,
+          params: _.merge({}, defaultOptions, queryArgs.params),
+        }),
+        providesTags: () => [{ type: TAGS.MESH_MODELS }],
+      }),
+    }),
+  });
+
+export const {
+  useLazyGetMeshModelsQuery,
+  useLazyGetComponentsQuery,
+  useLazyGetRelationshipsQuery,
+  useLazyGetRegistrantsQuery,
+  useLazyGetComponentsFromModalQuery,
+  useLazyGetRelationshipsFromModalQuery,
+  useGetModelCategoriesQuery,
+  useLazyGetModelFromCategoryQuery,
+} = meshModelApi;
