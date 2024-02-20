da3eb1971ccb23076bb1cbc17181ac37ebb6e258
Antonette Caldwell <acald.nebula@gmail.com>
2024-02-20 07:52:46 -0600

    feat: add @mui/material-nextjs package
    Signed-off-by: Antonette Caldwell <acald.nebula@gmail.com>

--- examples/next-14/pages/_app.jsx
+++ examples/next-14/pages/_app.jsx
@@ -1,13 +1,18 @@
 import { store } from '@/lib/redux';
+import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
 import { SistentThemeProvider } from '@layer5/sistent';
 import { Provider } from 'react-redux';
 
-export default function App({ Component, pageProps }) {
+export default function App({ ...props }) {
+  const { Component, pageProps } = props;
+
   return (
     <Provider store={store()}>
-      <SistentThemeProvider>
-        <Component {...pageProps} />
-      </SistentThemeProvider>
+      <AppCacheProvider {...props}>
+        <SistentThemeProvider>
+          <Component {...pageProps} />
+        </SistentThemeProvider>
+      </AppCacheProvider>
     </Provider>
   );
 }
