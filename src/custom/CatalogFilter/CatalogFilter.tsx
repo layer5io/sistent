a1f77bf695357ddf9bc54f60089de375ff2559f9
Rudraksh Tyagi <rudraksh875@gmail.com>
2024-03-04 14:07:18 +0530

    chore: move icon styles to constants directory
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

--- src/custom/CatalogFilter/CatalogFilter.tsx
+++ src/custom/CatalogFilter/CatalogFilter.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import Button from '@mui/material/Button';
-import { iconMedium } from '../../css/icons.styles';
+import { iconMedium } from '../../constants/icons.styles';
 
 export interface CatalogFilterProps {
   catalogVisibility: boolean;
