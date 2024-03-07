ed3ffc2db4e912881ea1fa199c08e195913055da
Abid <abid@Abids-MacBook-Air.local>
2024-03-08 04:39:18 +0530

    chore(theme): add interfaces for tokens and add few
    Signed-off-by: Abid <abid@Abids-MacBook-Air.local>

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -1,133 +1,68 @@
-import { PaletteOptions, lighten } from '@mui/material';
-import {
-  CHARCOAL,
-  KEPPEL,
-  accentGrey,
-  actionIcon,
-  buttonDelete,
-  common,
-  cultured,
-  notificationColors
-} from './colors';
+import { PaletteOptions } from '@mui/material';
+import * as Colors from './colors';
 
 declare module '@mui/material/styles' {
+  interface Interactiveness {
+    default: string;
+    hover: string;
+    disabled: string;
+    pressed: string;
+    secondary: string;
+    tertiary: string;
+  }
+
   interface PaletteColor {
-    darker?: string;
+    background: {
+      default: string;
+      primary: string;
+      secondary: string;
+      tertiary: string;
+      hover: string;
+      blur: {
+        heavy: string;
+        light: string;
+      };
+      brand: Interactiveness;
+      cta: Interactiveness;
+      info: Interactiveness;
+      success: Interactiveness;
+      warning: Interactiveness;
+      error: Interactiveness;
+      code: string;
+    };
   }
   interface SimplePaletteColorOptions {
-    darker?: string;
-  }
-  interface Palette {
-    neutral?: Palette['primary'];
-    cultured: {
-      main: string;
-    };
-    actionIcon: {
-      main: string;
+    background: {
+      default: string;
+      primary: string;
+      secondary: string;
+      tertiary: string;
       hover: string;
-    };
-    buttonDelete: {
-      main: string;
-      hover: string;
-    };
-  }
-  interface PaletteOptions {
-    neutral?: PaletteOptions['primary'];
-    cultured?: {
-      main?: string;
-    };
-    actionIcon?: {
-      main?: string;
-      hover?: string;
-    };
-    buttonDelete?: {
-      main?: string;
-      hover?: string;
+      blur: {
+        heavy: string;
+        light: string;
+      };
+      brand: Interactiveness;
+      cta: Interactiveness;
+      info: Interactiveness;
+      success: Interactiveness;
+      warning: Interactiveness;
+      error: Interactiveness;
+      code: string;
     };
   }
 }
 
 export const lightModePalette: PaletteOptions = {
-  primary: {
-    main: CHARCOAL,
-    light: lighten(CHARCOAL, 0.7),
-    contrastText: common.white
-  },
-  secondary: {
-    main: KEPPEL
-  },
   background: {
-    default: accentGrey[90],
-    paper: accentGrey[90]
-  },
-  info: {
-    main: notificationColors.info.main
-  },
-  error: {
-    main: notificationColors.error.main,
-    dark: notificationColors.error.dark
-  },
-  warning: {
-    main: notificationColors.warning.main,
-    light: notificationColors.warning.light,
-    contrastText: common.white
-  },
-  success: {
-    main: notificationColors.success.main
-  },
-  common: {
-    black: common.black,
-    white: common.white
-  },
-  cultured: {
-    main: cultured.main
-  },
-  actionIcon: {
-    main: actionIcon.main,
-    hover: actionIcon.hover
-  },
-  buttonDelete: {
-    main: buttonDelete.main,
-    hover: buttonDelete.hover
-  },
-  neutral: {},
-  text: {}
+    default: Colors.charcoal[100],
+    secondary: Colors.accentGrey[90],
+    tertiary: Colors.accentGrey[80]
+    // rest of token in background
+  }
+  // rest of tokens
 };
 
 export const darkModePalette: PaletteOptions = {
-  primary: {
-    main: KEPPEL,
-    light: lighten(KEPPEL, 0.7),
-    contrastText: common.white
-  },
-  secondary: {
-    main: KEPPEL
-  },
-  background: {
-    default: accentGrey[10],
-    paper: accentGrey[10]
-  },
-  info: {
-    main: notificationColors.info.main,
-    contrastText: common.white
-  },
-  error: {
-    main: notificationColors.error.main,
-    dark: notificationColors.error.dark,
-    contrastText: common.white
-  },
-  warning: {
-    main: notificationColors.warning.main,
-    light: notificationColors.warning.light,
-    contrastText: common.white
-  },
-  success: {
-    main: notificationColors.success.main,
-    contrastText: common.white
-  },
-  common: {
-    black: common.black,
-    white: common.white
-  },
-  neutral: {}
+  // dark variants of the tokens
 };
