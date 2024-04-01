8dd231c4719267d49607032f3a64114d9b8631de
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2024-04-01 14:18:02 +0530

    feat(Typography): add typography
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,72 +1,123 @@
-import { PaletteMode } from '@mui/material';
+import { PaletteMode, createTheme } from '@mui/material';
 import { TypographyOptions } from '@mui/material/styles/createTypography';
 import { common } from './colors';
+
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    textH1Bold: React.CSSProperties;
+    textH2Medium: React.CSSProperties;
+    textH3Medium: React.CSSProperties;
+    textB1Regular: React.CSSProperties;
+    textB2SemiBold: React.CSSProperties;
+    textB3Regular: React.CSSProperties;
+    textL1Bold: React.CSSProperties;
+    textL2Regular: React.CSSProperties;
+    textC1Regular: React.CSSProperties;
+    textC2Regular: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    textH1Bold: React.CSSProperties;
+    textH2Medium: React.CSSProperties;
+    textH3Medium: React.CSSProperties;
+    textB1Regular: React.CSSProperties;
+    textB2SemiBold: React.CSSProperties;
+    textB3Regular: React.CSSProperties;
+    textL1Bold: React.CSSProperties;
+    textL2Regular: React.CSSProperties;
+    textC1Regular: React.CSSProperties;
+    textC2Regular: React.CSSProperties;
+  }
+}
+
+const theme = createTheme();
 
 export const typography = (mode: PaletteMode): TypographyOptions => {
   return {
     fontFamily: ['Qanelas Soft Regular', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
-    body1: {
-      fontFamily: ['Open Sans', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
-      fontSize: '16px'
-    },
-    body2: {
-      fontFamily: ['Open Sans', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
-      fontSize: '16px',
-      fontWeight: 'normal',
-      lineHeight: '28px'
-    },
-    h1: {
-      fontFamily: ['Qanelas Soft Regular', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
-      fontSize: '64px',
-      lineHeight: '80px',
-      fontWeight: 'bold'
-    },
-    h2: {
+    textH1Bold: {
       fontFamily: ['Qanelas Soft Regular', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
       fontSize: '52px',
       lineHeight: '64px',
-      fontWeight: 'bold'
+      fontWeight: 'bold',
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '32px',
+        lineHeight: '40px'
+      }
     },
-    h3: {
+    textH2Medium: {
       fontFamily: ['Qanelas Soft Regular', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
       fontSize: '32px',
       lineHeight: '40px',
-      fontWeight: 'bold'
+      fontWeight: 'medium',
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '24px',
+        lineHeight: '36px'
+      }
     },
-    h4: {
+    textH3Medium: {
       fontFamily: ['Qanelas Soft Regular', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
       fontSize: '1.5rem',
       lineHeight: '2.25rem',
-      fontWeight: 600
+      fontWeight: 'medium',
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '1rem',
+        lineHeight: '1.75rem'
+      }
     },
-    h5: {
-      fontFamily: ['Qanelas Soft Regular', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
+    textB1Regular: {
+      fontFamily: ['Open Sans'].join(','),
       color: mode === 'light' ? common.black : common.white,
       fontSize: '1rem',
+      fontWeight: 'regular',
+      lineHeight: '1.75rem',
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '1rem',
+        lineHeight: '1.75rem'
+      }
+    },
+    textB2SemiBold: {
+      fontFamily: ['Open Sans'].join(','),
+      color: mode === 'light' ? common.black : common.white,
+      fontSize: '1rem',
+      fontWeight: 'semi-bold',
+      lineHeight: '1.75rem',
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '1rem',
+        lineHeight: '1.75rem'
+      }
+    },
+    textB3Regular: {
+      fontFamily: ['Open Sans', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
+      fontSize: '0.875rem',
+      fontWeight: 'regular',
+      lineHeight: '1.5rem'
+    },
+    textL1Bold: {
+      fontFamily: ['Qanelas Soft'].join(','),
+      fontSize: '0.75rem',
       fontWeight: 'bold',
+      lineHeight: '1rem'
+    },
+    textL2Regular: {
+      fontFamily: ['Open Sans'].join(','),
+      fontSize: '0.75rem',
+      fontWeight: 'regular',
+      lineHeight: '1.5rem'
+    },
+    textC1Regular: {
+      fontFamily: ['Consolas'].join(','),
+      fontSize: '0.75rem',
+      fontWeight: 'regular',
+      lineHeight: '1.5rem'
+    },
+    textC2Regular: {
+      fontFamily: ['Consolas'].join(','),
+      fontSize: '1rem',
+      fontWeight: 'regular',
       lineHeight: '1.75rem'
     },
-    h6: {
-      fontFamily: ['Qanelas Soft Regular', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
-      color: mode === 'light' ? common.black : common.white,
-      fontSize: '12px',
-      fontWeight: 'bold',
-      lineHeight: '16px',
-      textTransform: 'uppercase'
-    },
-    subtitle1: {
-      fontFamily: ['Open Sans', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
-      fontSize: '16px',
-      fontWeight: 500,
-      lineHeight: '28px'
-    },
-    subtitle2: {
-      fontFamily: ['Open Sans', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
-      fontSize: '12px',
-      fontWeight: 500,
-      lineHeight: '24px'
-    },
-    button: {},
     caption: {},
     overline: {}
   };
