32f988ec6ff84df667a1794713858879efaf69f8
captain-Akshay <akshay.sharma0503@gmail.com>
2024-04-06 13:00:36 +0530

    feat(button): feedbackbutton & action button
    Signed-off-by: captain-Akshay <akshay.sharma0503@gmail.com>

--- src/custom/ActionButton/ActionButton.tsx
+++ src/custom/ActionButton/ActionButton.tsx
@@ -3,14 +3,12 @@ import {
   Button,
   ButtonGroup,
   ClickAwayListener,
-  Grow,
   MenuItem,
   MenuList,
   Paper,
   Popper
 } from '../../base';
-import DropDownIcon from '../../icons';
-
+import { DropDownIcon } from '../../icons';
 interface Option {
   icon: React.ReactNode;
   label: string;
@@ -20,29 +18,28 @@ interface Option {
 interface ActionButtonProps {
   defaultActionClick: () => void;
   options: Option[];
+  label: string;
 }
 
 export default function ActionButton({
   defaultActionClick,
-  options
+  options,
+  label
 }: ActionButtonProps): JSX.Element {
   const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef<HTMLDivElement>(null);
-
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const handleMenuItemClick = () => {
     setOpen(false);
   };
 
   const handleToggle = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.stopPropagation();
+    setAnchorEl(event.currentTarget);
     setOpen((prevOpen) => !prevOpen);
   };
 
-  const handleClose = (event: MouseEvent | TouchEvent) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target as Node)) {
-      return;
-    }
-
+  const handleClose = () => {
+    setAnchorEl(null);
     setOpen(false);
   };
 
@@ -51,11 +48,10 @@ export default function ActionButton({
       <ButtonGroup
         variant="contained"
         style={{ boxShadow: 'none' }}
-        ref={anchorRef}
         aria-label="Button group with a nested menu"
       >
         <Button onClick={defaultActionClick} variant="contained">
-          Action
+          {label}
         </Button>
         <Button size="small" onClick={handleToggle} variant="contained">
           <DropDownIcon />
@@ -66,38 +62,27 @@ export default function ActionButton({
           zIndex: 1
         }}
         open={open}
-        anchorEl={anchorRef.current}
+        anchorEl={anchorEl}
         role={undefined}
-        transition
-        disablePortal
       >
-        {({ TransitionProps, placement }) => (
-          <Grow
-            {...TransitionProps}
-            style={{
-              transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom'
-            }}
-          >
-            <Paper>
-              <ClickAwayListener onClickAway={handleClose}>
-                <MenuList id="split-button-menu" autoFocusItem>
-                  {options.map((option, index) => (
-                    <MenuItem
-                      key={index}
-                      onClick={(event) => {
-                        handleMenuItemClick();
-                        option.onClick(event, index);
-                      }}
-                    >
-                      <div style={{ marginRight: '1rem' }}>{option.icon}</div>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-          </Grow>
-        )}
+        <Paper>
+          <ClickAwayListener onClickAway={handleClose}>
+            <MenuList id="split-button-menu" autoFocusItem>
+              {options.map((option, index) => (
+                <MenuItem
+                  key={index}
+                  onClick={(event) => {
+                    handleMenuItemClick();
+                    option.onClick(event, index);
+                  }}
+                >
+                  <div style={{ marginRight: '1rem' }}>{option.icon}</div>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </ClickAwayListener>
+        </Paper>
       </Popper>
     </React.Fragment>
   );
--- src/icons/DropDownIcon/DropDownIcon.tsx
+++ src/icons/DropDownIcon/DropDownIcon.tsx
@@ -12,7 +12,7 @@ export const DropDownIcon = ({
       viewBox="-6.5 0 32 32"
       width={width}
       height={height}
-      fill={props.fill}
+      fill={props.fill || 'currentColor'}
       {...props}
     >
       <path d="M18.813 11.406l-7.906 9.906c-0.75 0.906-1.906 0.906-2.625 0l-7.906-9.906c-0.75-0.938-0.375-1.656 0.781-1.656h16.875c1.188 0 1.531 0.719 0.781 1.656z"></path>
