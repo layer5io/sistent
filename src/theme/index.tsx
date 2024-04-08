23ae2069b08a72a305597bf460042dafcc74a439
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2024-04-08 12:52:48 +0530

    fix(add): index
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,4 +1,5 @@
 export { darkModePalette, lightModePalette } from './palette';
+export { typography } from './typography';
 
 export { default as SistentThemeProvider } from './ThemeProvider';
 export * from './colors';
