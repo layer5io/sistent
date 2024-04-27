a33fedd8bc44ba1dd47dfbea5069d6177623880d
Antonette Caldwell <134739862+nebula-aac@users.noreply.github.com>
2024-04-27 19:06:27 +0000

    feat: create typopgraphy styled components from existing options
    Signed-off-by: Antonette Caldwell <134739862+nebula-aac@users.noreply.github.com>

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -53,22 +53,24 @@ export const typography = (mode: PaletteMode): TypographyOptions => {
     fontFamily: ['Qanelas Soft Regular', 'Roboto', 'Helvectica', 'Arial', 'sans-serif'].join(','),
     textH1Bold: {
       fontFamily: ['Qanelas Soft Regular'].join(','),
-      fontSize: '52px',
-      lineHeight: '64px',
+      fontSize: '3.25rem',
+      lineHeight: '4rem',
       fontWeight: 700,
+      display: 'block',
       [theme.breakpoints.down('sm')]: {
-        fontSize: '32px',
-        lineHeight: '40px'
+        fontSize: '2rem',
+        lineHeight: '2.5rem'
       }
     },
     textH2Medium: {
       fontFamily: ['Qanelas Soft Regular'].join(','),
-      fontSize: '32px',
-      lineHeight: '40px',
+      fontSize: '2rem',
+      lineHeight: '2.5rem',
       fontWeight: 500,
+      display: 'block',
       [theme.breakpoints.down('sm')]: {
-        fontSize: '24px',
-        lineHeight: '36px'
+        fontSize: '1.5rem',
+        lineHeight: '2.25rem'
       }
     },
     textH3Medium: {
@@ -76,6 +78,7 @@ export const typography = (mode: PaletteMode): TypographyOptions => {
       fontSize: '1.5rem',
       lineHeight: '2.25rem',
       fontWeight: 500,
+      display: 'block',
       [theme.breakpoints.down('sm')]: {
         fontSize: '1rem',
         lineHeight: '1.75rem',
@@ -83,57 +86,64 @@ export const typography = (mode: PaletteMode): TypographyOptions => {
       }
     },
     textB1Regular: {
-      fontFamily: ['Open Sans'].join(','),
+      fontFamily: ['"Open Sans"', 'sans-serif'].join(','),
       color: mode === 'light' ? common.black : common.white,
       fontSize: '1rem',
       fontWeight: 400,
       lineHeight: '1.75rem',
+      display: 'block',
       [theme.breakpoints.down('sm')]: {
         fontSize: '1rem',
         lineHeight: '1.75rem'
       }
     },
     textB2SemiBold: {
-      fontFamily: ['Open Sans'].join(','),
+      fontFamily: ['"Open Sans"', 'sans-serif'].join(','),
       color: mode === 'light' ? common.black : common.white,
       fontSize: '1rem',
       fontWeight: 600,
       lineHeight: '1.75rem',
       textTransform: 'capitalize',
+      display: 'block',
       [theme.breakpoints.down('sm')]: {
         fontSize: '1rem',
         lineHeight: '1.75rem'
       }
     },
     textB3Regular: {
-      fontFamily: ['Open Sans'].join(','),
+      fontFamily: ['"Open Sans"', 'sans-serif'].join(','),
       fontSize: '0.875rem',
       fontWeight: 400,
-      lineHeight: '1.5rem'
+      lineHeight: '1.5rem',
+      display: 'block'
     },
     textL1Bold: {
       fontFamily: ['Qanelas Soft Regular'].join(','),
       fontSize: '0.75rem',
       fontWeight: 700,
-      lineHeight: '1rem'
+      lineHeight: '1rem',
+      display: 'block'
     },
     textL2Regular: {
-      fontFamily: ['Open Sans'].join(','),
+      fontFamily: ['"Open Sans"', 'sans-serif'].join(','),
       fontSize: '0.75rem',
       fontWeight: 400,
-      lineHeight: '1.5rem'
+      lineHeight: '1.5rem',
+      display: 'block'
     },
     textC1Regular: {
-      fontFamily: ['Consolas'].join(','),
+      fontFamily: ['Consolas', 'monospace'].join(','),
       fontSize: '0.75rem',
       fontWeight: 400,
-      lineHeight: '1.5rem'
+      lineHeight: '1.5rem',
+      display: 'block'
     },
     textC2Regular: {
-      fontFamily: ['Consolas'].join(','),
+      fontFamily: ['Consolas', 'monospace'].join(','),
       fontSize: '1rem',
       fontWeight: 400,
-      lineHeight: '1.75rem'
+      lineHeight: '1.75rem',
+      display: 'block'
     },
     caption: {},
     overline: {}
