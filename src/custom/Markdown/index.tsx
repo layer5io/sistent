9dd11e82b39ad768f4299cc6f9e73c1bfa4a2a07
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2024-05-12 00:13:56 +0530

    feat(elements): styles
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- src/custom/Markdown/index.tsx
+++ src/custom/Markdown/index.tsx
@@ -1,6 +1,20 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { StyledMarkdown } from './style';
+import {
+  StyledMarkdown,
+  StyledMarkdownBlockquote,
+  StyledMarkdownH1,
+  StyledMarkdownH2,
+  StyledMarkdownH3,
+  StyledMarkdownH4,
+  StyledMarkdownH5,
+  StyledMarkdownH6,
+  StyledMarkdownLi,
+  StyledMarkdownP,
+  StyledMarkdownTd,
+  StyledMarkdownTh,
+  StyledMarkdownUl
+} from './style';
 export interface RenderMarkdownProps {
   content: string;
 }
@@ -10,7 +24,21 @@ const RenderMarkdown: React.FC<RenderMarkdownProps> = ({ content }) => {
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
       components={{
-        a: ({ ...props }) => <StyledMarkdown>{props.children}</StyledMarkdown>
+        a: ({ ...props }) => <StyledMarkdown>{props.children}</StyledMarkdown>,
+        p: ({ ...props }) => <StyledMarkdownP>{props.children}</StyledMarkdownP>,
+        h1: ({ ...props }) => <StyledMarkdownH1>{props.children}</StyledMarkdownH1>,
+        h2: ({ ...props }) => <StyledMarkdownH2>{props.children}</StyledMarkdownH2>,
+        h3: ({ ...props }) => <StyledMarkdownH3>{props.children}</StyledMarkdownH3>,
+        h4: ({ ...props }) => <StyledMarkdownH4>{props.children}</StyledMarkdownH4>,
+        h5: ({ ...props }) => <StyledMarkdownH5>{props.children}</StyledMarkdownH5>,
+        h6: ({ ...props }) => <StyledMarkdownH6>{props.children}</StyledMarkdownH6>,
+        blockquote: ({ ...props }) => (
+          <StyledMarkdownBlockquote>{props.children}</StyledMarkdownBlockquote>
+        ),
+        ul: ({ ...props }) => <StyledMarkdownUl>{props.children}</StyledMarkdownUl>,
+        li: ({ ...props }) => <StyledMarkdownLi>{props.children}</StyledMarkdownLi>,
+        th: ({ ...props }) => <StyledMarkdownTh>{props.children}</StyledMarkdownTh>,
+        td: ({ ...props }) => <StyledMarkdownTd>{props.children}</StyledMarkdownTd>
       }}
     >
       {content}
