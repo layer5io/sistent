7952cceb7b2ee4bef7da3a016663cc02fefeee7c
rishabhsharma1997 <rishabhsharma19997@gmail.com>
2024-06-21 05:30:24 +0530

    fix: ul line for TOC
    Signed-off-by: rishabhsharma1997 <rishabhsharma19997@gmail.com>

--- src/custom/TOCChapter/style.tsx
+++ src/custom/TOCChapter/style.tsx
@@ -44,7 +44,7 @@ export const TOCWrapper = styled('div')(({ theme }) => ({
       MozPaddingStart: '2.65rem',
       '&::after': {
         position: 'absolute',
-        inset: '1rem auto 1rem 31px',
+        inset: '1rem auto 1rem 27px',
         width: 'auto',
         height: 'auto',
         borderLeft: `1px solid rgba(177, 182, 184, 0.25)`,
@@ -78,50 +78,10 @@ export const TOCWrapper = styled('div')(({ theme }) => ({
       }
     }
   },
-  '@media(max-width: 992px)': {
+  '@media(max-width: 1200px)': {
     '.toc-list ul': {
       '&::after': {
-        inset: '1rem auto 1rem 32.4px'
-      }
-    }
-  },
-  '@media(max-width: 767px)': {
-    position: 'initial',
-    '.toc-list ul': {
-      display: 'flex',
-      flexFlow: 'wrap',
-      margin: '1.5rem 0',
-      flexDirection: 'column',
-      paddingInlineStart: '0rem',
-      '&::after': {
-        display: 'none'
-      },
-      '& li': {
-        listStyleType: 'none',
-        margin: '0.5rem',
-        display: 'none'
-      }
-    },
-    '.toc-ul': {
-      opacity: 0,
-      height: 0,
-      transition: 'none',
-      paddingLeft: '1rem'
-    },
-    '.toc-ul-open': {
-      height: 'auto',
-      opacity: 1,
-      transition: 'all .4s',
-      '& li': {
-        display: 'inline-block'
-      }
-    },
-    '.chapter-back': {
-      '& h4': {
-        margin: '0 1rem'
-      },
-      '.toc-toggle-btn': {
-        display: 'flex'
+        inset: '1rem auto 1rem 24px'
       }
     }
   }
