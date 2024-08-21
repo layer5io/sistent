0ea46e01d377c5ad7b7ee8a152315d1d523815e3
Senthil Athiban <senthilathiban0@gmail.com>
2024-08-21 10:28:48 +0530

    fix(lint): lint issue
    Signed-off-by: Senthil Athiban <senthilathiban0@gmail.com>

--- src/custom/SearchBar.tsx
+++ src/custom/SearchBar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { outlinedInputClasses } from '@mui/material/OutlinedInput';
 import { Theme, ThemeProvider, createTheme, useTheme } from '@mui/material/styles';
 import debounce from 'lodash/debounce';
