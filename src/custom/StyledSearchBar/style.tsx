7e0eedc1a873ba81c4cc96af46a39e5799dfa6c0
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-10-11 18:51:48 +0530

    feat: update border color of the search that in cloud
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- src/custom/StyledSearchBar/style.tsx
+++ src/custom/StyledSearchBar/style.tsx
@@ -1,18 +1,19 @@
 import { styled } from '@mui/material';
 import { InputAdornment, OutlinedInput } from '../../base';
 
-export const StyledSearchInput = styled(OutlinedInput)(({ style }) => ({
+export const StyledSearchInput = styled(OutlinedInput)(({ style, theme }) => ({
   width: '100%',
   '@media (max-width: 590px)': {
     marginLeft: '0.25rem',
     paddingLeft: '0.25rem'
   },
   display: 'flex',
+  backgroundColor: theme.palette.background.surfaces,
   ...style
 }));
 
 export const InputAdornmentEnd = styled(InputAdornment)(({ theme }) => ({
-  borderLeft: `1px solid ${theme.palette.border.normal}`,
+  borderLeft: `1px solid ${theme.palette.background.tertiary}`,
   height: '30px',
   paddingLeft: '10px',
   '@media (max-width: 590px)': {
