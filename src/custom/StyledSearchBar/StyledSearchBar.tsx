1149a324332805023acd957212f721bec1df543a
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-10-19 19:28:31 +0530

    fix: lint failed issue
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- src/custom/StyledSearchBar/StyledSearchBar.tsx
+++ src/custom/StyledSearchBar/StyledSearchBar.tsx
@@ -48,6 +48,7 @@ function StyledSearchBar({
     if (value !== inputValue) {
       setInputValue(value);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value]);
 
   // Create synthetic event helper
