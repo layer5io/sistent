721c1b5fa318538bdece1237027f8ffa7299e92f
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-11-11 13:30:09 +0530

    feat: export GridViewIcon and TableViewIcon components; update CatalogDesignTable exports
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- src/custom/CatalogDesignTable/index.ts
+++ src/custom/CatalogDesignTable/index.ts
@@ -1,3 +1,5 @@
 import CatalogDesignsTable from './CatalogDesignTable';
 import { colViews, createDesignColumns } from './columnConfig';
+export { CatalogTableVisibilityControl } from './CatalogTableVisibilityControl';
+export { ViewSwitch } from './ViewSwitch';
 export { CatalogDesignsTable, colViews, createDesignColumns };
