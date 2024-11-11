eef44ea18bb01d0c2e646de6c92a50e050e37428
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-11-11 12:40:55 +0530

    fix: issue in the catalog design table
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- src/custom/CatalogDesignTable/CatalogDesignTable.tsx
+++ src/custom/CatalogDesignTable/CatalogDesignTable.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import _ from 'lodash';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { PublishIcon } from '../../icons';
+import { CHARCOAL, useTheme } from '../../theme';
 import { Pattern } from '../CustomCatalog/CustomCard';
 import { useWindowDimensions } from '../Helpers/Dimension';
+import PromptComponent from '../Prompt';
+import { PromptRef } from '../Prompt/promt-component';
 import ResponsiveDataTable from '../ResponsiveDataTable';
+import UnpublishTooltipIcon from './UnpublishTooltipIcon';
 
 interface CatalogDesignsTableProps {
-  patters: Pattern[];
+  patterns: Pattern[];
   filter: any;
   columns: Array<any>;
   totalCount: number;
@@ -17,11 +22,17 @@ interface CatalogDesignsTableProps {
   page: number;
   setPage: (page: number) => void;
   columnVisibility: Record<string, boolean>;
-  colViews: any;
+  colViews: Record<string, boolean> | undefined;
+  handleBulkDeleteModal: (patterns: Pattern[], modalRef: React.RefObject<PromptRef>) => void;
+  handleBulkpatternsDataUnpublishModal: (
+    selected: any,
+    patterns: Pattern[],
+    modalRef: React.RefObject<PromptRef>
+  ) => void;
 }
 
 export const CatalogDesignsTable: React.FC<CatalogDesignsTableProps> = ({
-  patters,
+  patterns,
   filter,
   columns = [],
   totalCount = 0,
@@ -32,12 +43,15 @@ export const CatalogDesignsTable: React.FC<CatalogDesignsTableProps> = ({
   page = 0,
   setPage,
   columnVisibility = {},
-  colViews = {}
+  colViews = {},
+  handleBulkDeleteModal,
+  handleBulkpatternsDataUnpublishModal
 }) => {
   const [tableCols, updateCols] = useState<Array<any>>([]);
   const { width } = useWindowDimensions();
   const smallScreen = width <= 360;
-  // const modalRef = useRef(null);
+  const theme = useTheme();
+  const modalRef = useRef<PromptRef>(null);
 
   useEffect(() => {
     if (Array.isArray(columns) && columns.length > 0) {
@@ -45,7 +59,7 @@ export const CatalogDesignsTable: React.FC<CatalogDesignsTableProps> = ({
     }
   }, [columns]);
 
-  const options = {
+  const options: any = {
     selectableRows: _.isNil(filter) ? 'none' : 'multiple',
     serverSide: true,
     filterType: 'multiselect',
@@ -54,15 +68,12 @@ export const CatalogDesignsTable: React.FC<CatalogDesignsTableProps> = ({
     rowsPerPage: pageSize,
     page,
     elevation: 0,
-    search: false,
-    filter: false,
-    download: false,
-    print: false,
-    viewColumns: false,
     onTableChange: (action: string, tableState: any) => {
       const sortInfo = tableState.announceText ? tableState.announceText.split(' : ') : [];
       let order = '';
-
+      if (tableState.activeColumn) {
+        order = `${columns[tableState.activeColumn].name} desc`;
+      }
       switch (action) {
         case 'changePage':
           setPage(tableState.page);
@@ -90,20 +101,49 @@ export const CatalogDesignsTable: React.FC<CatalogDesignsTableProps> = ({
     }
   };
 
+  if (_.isNil(filter)) {
+    options.customToolbarSelect = (selected: any) => (
+      <UnpublishTooltipIcon
+        title="Unpublish"
+        onClick={() => handleBulkpatternsDataUnpublishModal(selected, patterns, modalRef)}
+        iconType="publish"
+        id={'unpublish-button'}
+      >
+        <PublishIcon width={28.8} height={28.8} fill={CHARCOAL} />
+      </UnpublishTooltipIcon>
+    );
+  } else {
+    options.onRowsDelete = (rowsDeleted: any) => {
+      const selectedPatterns = rowsDeleted.data.map(({ dataIndex }: any) => patterns[dataIndex]);
+      handleBulkDeleteModal(selectedPatterns, modalRef);
+      return false;
+    };
+  }
+
   if (!Array.isArray(tableCols) || tableCols.length === 0) {
     return null;
   }
 
   return (
-    <ResponsiveDataTable
-      columns={tableCols ?? []}
-      data={patters.map((pattern) => Object.values(pattern)) ?? []}
-      options={options}
-      colViews={colViews}
-      tableCols={tableCols}
-      updateCols={updateCols}
-      columnVisibility={columnVisibility}
-    />
+    <>
+      <PromptComponent ref={modalRef} />
+      <ResponsiveDataTable
+        columns={columns}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        data={patterns || []}
+        options={options}
+        colViews={colViews}
+        tableCols={tableCols}
+        updateCols={updateCols}
+        columnVisibility={columnVisibility}
+        backgroundColor={
+          theme.palette.mode === 'light'
+            ? theme.palette.background.default
+            : theme.palette.background.secondary
+        }
+      />
+    </>
   );
 };
 
