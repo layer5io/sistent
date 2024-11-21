6d35b8f9f6bf50be2cee3d457210342cfeb779ff
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-11-21 23:43:26 +0530

    feat(UI): add FormLabel component
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

---
+++ src/base/FormLabel/FormLabel.tsx
@@ -0,0 +1,5 @@
+import { FormLabel as MuiFormLabel, FormLabelProps as MuiFormLabelProps } from '@mui/material';
+
+export function FormLabel(props: MuiFormLabelProps): JSX.Element {
+  return <MuiFormLabel {...props} />;
+}
