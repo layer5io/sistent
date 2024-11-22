37933af1aa126e37f8966a362e64482149df93d2
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-11-22 17:41:44 +0530

    feat: add EditButton component with custom tooltip and styled icon
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- src/custom/Workspaces/styles.ts
+++ src/custom/Workspaces/styles.ts
@@ -1,6 +1,34 @@
-import { styled } from '../../theme';
+import EditIcon from '@mui/icons-material/Edit';
+import { buttonDisabled, styled } from '../../theme';
+import { HOVER_DELETE } from '../../theme/colors/colors';
 
 export const ModalActionDiv = styled('div')({
   display: 'flex',
   gap: '1rem'
 });
+
+interface ExtendedEditIconProps {
+  disabled?: boolean;
+  bulk?: boolean;
+  style?: React.CSSProperties;
+}
+
+export const L5EditIcon = styled(EditIcon)<ExtendedEditIconProps>(
+  ({ disabled, bulk, style, theme }) => ({
+    color: disabled ? theme.palette.icon.disabled : theme.palette.text.secondary,
+    cursor: disabled ? 'not-allowed' : 'pointer',
+    width: bulk ? '32' : '28.8',
+    height: bulk ? '32' : '28.8',
+    '&:hover': {
+      color: disabled ? buttonDisabled : HOVER_DELETE,
+      '& svg': {
+        color: disabled ? buttonDisabled : HOVER_DELETE
+      }
+    },
+    '& svg': {
+      color: theme.palette.error.main,
+      cursor: disabled ? 'not-allowed' : 'pointer'
+    },
+    ...style
+  })
+);
