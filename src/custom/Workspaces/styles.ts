43cf18b025b4386ba26b86646ea7ccb73130a5e6
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-11-25 14:52:20 +0530

    feat: remove TooltipIcon component and add helper functions for timestamp parsing
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- src/custom/Workspaces/styles.ts
+++ src/custom/Workspaces/styles.ts
@@ -63,3 +63,50 @@ export const CustomBodyRenderStyle = styled('div')({
   display: 'block',
   width: '100%'
 });
+
+export const TableTopIcon = styled('span')(() => ({
+  '& svg': {
+    cursor: 'pointer',
+    width: '2rem',
+    height: '2rem'
+  }
+}));
+
+export const DisabledTableTopIcon = styled('span')(() => ({
+  '& svg': {
+    width: '2rem',
+    height: '2rem'
+  }
+}));
+
+export const MesheryDeleteIcon = styled('span')(({ theme }) => ({
+  '& svg': {
+    color: '#3C494F',
+    '&:hover': {
+      color: theme.palette.error.error
+    }
+  }
+}));
+
+export const TableIconsDisabledContainer = styled('span')(() => ({
+  color: '#455a64',
+  opacity: '0.5',
+  '& svg': {
+    cursor: 'not-allowed'
+  }
+}));
+
+export const TableTopIconsWrapper = styled('div')(() => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  paddingRight: '26px'
+}));
+
+export const TableIconsContainer = styled('div')(() => ({
+  color: '#455a64',
+  display: 'flex',
+  cursor: 'not-allowed',
+  '& svg': {
+    cursor: 'pointer'
+  }
+}));
