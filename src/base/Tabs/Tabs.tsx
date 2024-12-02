5537f6baf9fea4845f73df3bcac0f11928727479
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-12-02 15:38:39 +0530

    style(tabs): add styling to tabs
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

--- src/base/Tabs/Tabs.tsx
+++ src/base/Tabs/Tabs.tsx
@@ -1,7 +1,33 @@
 import { Tabs as MuiTabs, type TabsProps as MuiTabsProps } from '@mui/material';
 
 export function Tabs(props: MuiTabsProps): JSX.Element {
-  return <MuiTabs {...props} />;
+  return (
+    <MuiTabs
+      {...props}
+      sx={(theme) => ({
+        width: '100%',
+        marginLeft: 0,
+        '& .MuiTabs-indicator': {
+          backgroundColor:
+            theme.palette.mode === 'dark'
+              ? theme.palette.background?.default
+              : theme.palette.background?.brand?.hover
+        },
+        '& .MuiTab-root': {
+          backgroundColor: 'transparent',
+          color:
+            theme.palette.mode === 'dark' ? theme.palette.text.default : theme.palette.text.default,
+          '&.Mui-selected': {
+            backgroundColor: 'transparent',
+            color:
+              theme.palette.mode === 'dark'
+                ? theme.palette.background?.brand
+                : theme.palette.background?.brand?.default
+          }
+        }
+      })}
+    />
+  );
 }
 
 export default Tabs;
