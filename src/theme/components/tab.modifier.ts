b3483338a41ea368e41fff6e12320cfb7f25c902
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-12-16 19:31:38 +0530

    feat(ui): updating theme
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

--- src/theme/components/tab.modifier.ts
+++ src/theme/components/tab.modifier.ts
@@ -1,17 +1,14 @@
 import { Components, Theme } from '@mui/material';
-import { GRAY } from '../colors';
+import { GRAY, WHITE } from '../colors';
 
 export const MuiTab: Components<Theme>['MuiTab'] = {
   styleOverrides: {
     root: ({ theme }) => ({
       backgroundColor: 'none',
-      color: theme.palette.text.primary,
+      color: theme.palette.mode === 'dark' ? WHITE : GRAY,
       '&.Mui-selected': {
         backgroundColor: 'none',
         color: theme.palette.mode === 'dark' ? GRAY : GRAY
-      },
-      '&:hover': {
-        backgroundColor: 'rgba(0, 0, 0, 0.04)'
       }
     })
   }
