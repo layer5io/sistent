8a0b38dac77345f6dbe25f8b0f0d0f627a595542
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-12-16 18:53:26 +0530

    feat(ui): updating theme
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

--- src/theme/components/tab.modifier.ts
+++ src/theme/components/tab.modifier.ts
@@ -4,10 +4,10 @@ import { GRAY } from '../colors';
 export const MuiTab: Components<Theme>['MuiTab'] = {
   styleOverrides: {
     root: ({ theme }) => ({
-      backgroundColor: 'transparent',
+      backgroundColor: 'none',
       color: theme.palette.text.primary,
       '&.Mui-selected': {
-        backgroundColor: 'transparent',
+        backgroundColor: 'none',
         color: theme.palette.mode === 'dark' ? GRAY : GRAY
       },
       '&:hover': {
