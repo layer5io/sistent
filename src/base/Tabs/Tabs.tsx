90bf8df1333d93eecf4006444874dffeb0023355
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-12-16 18:30:57 +0530

    feat(ui): removing unwanted code
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

--- src/base/Tabs/Tabs.tsx
+++ src/base/Tabs/Tabs.tsx
@@ -1,37 +1,3 @@
-// import { Tabs as MuiTabs, type TabsProps as MuiTabsProps } from '@mui/material';
-
-// export function Tabs(props: MuiTabsProps): JSX.Element {
-//   return (
-//     <MuiTabs
-//       {...props}
-//       sx={(theme) => ({
-//         width: '100%',
-//         marginLeft: 0,
-//         '& .MuiTabs-indicator': {
-//           backgroundColor:
-//             theme.palette.mode === 'dark'
-//               ? theme.palette.background?.default
-//               : theme.palette.background?.brand?.hover
-//         },
-//         '& .MuiTab-root': {
-//           backgroundColor: 'transparent',
-//           color:
-//             theme.palette.mode === 'dark' ? theme.palette.text.default : theme.palette.text.default,
-//           '&.Mui-selected': {
-//             backgroundColor: 'transparent',
-//             color:
-//               theme.palette.mode === 'dark'
-//                 ? theme.palette.background?.brand
-//                 : theme.palette.background?.brand?.default
-//           }
-//         }
-//       })}
-//     />
-//   );
-// }
-
-// export default Tabs;
-
 import { Tabs as MuiTabs, type TabsProps as MuiTabsProps } from '@mui/material';
 
 export function Tabs(props: MuiTabsProps): JSX.Element {
