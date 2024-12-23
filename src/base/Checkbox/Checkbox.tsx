b82a8b1f4580832214a44fea529df090b0c384cc
seniorliketocode <abhishek234543@gmail.com>
2024-12-23 17:09:19 +0530

    fix(CheckBox): typeError fixed
    Signed-off-by: seniorliketocode <abhishek234543@gmail.com>

--- src/base/Checkbox/Checkbox.tsx
+++ src/base/Checkbox/Checkbox.tsx
@@ -1,7 +1,7 @@
 import { Checkbox as MuiCheckbox, type CheckboxProps } from '@mui/material';
 import React from 'react';
 
-const Checkbox = React.forwardRef<HTMLDivElement, CheckboxProps>((props, ref) => {
+const Checkbox = React.forwardRef<HTMLButtonElement, CheckboxProps>((props, ref) => {
   return <MuiCheckbox {...props} ref={ref} />;
 });
 
