fdc64e4b1ec5e471a99532b764f3a824048ad539
seniorliketocode <abhishek234543@gmail.com>
2024-12-25 18:41:53 +0530

    fix(ListItemText): add forwardRef to ListItemText component
    Signed-off-by: seniorliketocode <abhishek234543@gmail.com>

--- src/base/ListItemText/ListItemText.tsx
+++ src/base/ListItemText/ListItemText.tsx
@@ -2,7 +2,10 @@ import {
   ListItemText as MuiListItemText,
   ListItemTextProps as MuiListItemTextProps
 } from '@mui/material';
+import React from 'react';
 
-export function ListItemText(props: MuiListItemTextProps): JSX.Element {
-  return <MuiListItemText {...props} />;
-}
+const ListItemText = React.forwardRef<HTMLDivElement, MuiListItemTextProps>((props, ref) => {
+  return <MuiListItemText {...props} ref={ref} />;
+});
+
+export { ListItemText };
