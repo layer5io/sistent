9cc0f2f4b410eeaa3e56325a6c4ef615531f1183
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2025-01-15 20:10:03 +0530

    fix(tabs): theme changes
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- src/theme/components/tabs.modifier.ts
+++ src/theme/components/tabs.modifier.ts
@@ -3,7 +3,7 @@ import { Components, Theme } from '@mui/material';
 export const MuiTabs: Components<Theme>['MuiTabs'] = {
   styleOverrides: {
     root: ({ theme }) => ({
-      backgroundColor: theme.palette.background.default
+      backgroundColor: theme.palette.background.neutral?.default
     }),
     indicator: ({ theme }) => ({
       backgroundColor: theme.palette.background.brand?.default
