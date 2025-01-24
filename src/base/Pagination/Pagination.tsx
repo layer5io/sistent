c724e7192a707c790993f03408f7409746d8e8da
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-01-24 20:04:26 +0530

    feat: add pagination item component to sistent
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- src/base/Pagination/Pagination.tsx
+++ src/base/Pagination/Pagination.tsx
@@ -1,8 +1,13 @@
-import { Pagination as MuiPagination, PaginationProps as MuiPaginationProps } from '@mui/material';
+import {
+  Pagination as MuiPagination,
+  PaginationItem as MuiPaginationItem,
+  PaginationProps as MuiPaginationProps
+} from '@mui/material';
 import React from 'react';
 
 const Pagination = React.forwardRef<HTMLDivElement, MuiPaginationProps>((props, ref) => {
   return <MuiPagination {...props} ref={ref} />;
 });
 
+export { MuiPaginationItem as PaginationItem };
 export default Pagination;
