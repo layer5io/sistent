5d5906e71b8a43668c083dab2ce833e91f477d71
amitamrutiya <amitamrutiya2210@gmail.com>
2025-02-18 18:02:57 +0530

    feat: add Hidden component and refactor IconButton for better ref handling
    Signed-off-by: amitamrutiya <amitamrutiya2210@gmail.com>

--- src/base/IconButton/IconButton.tsx
+++ src/base/IconButton/IconButton.tsx
@@ -1,5 +1,15 @@
-import { IconButton as MuiIconButton, type IconButtonProps } from '@mui/material';
+import {
+  IconButton as MuiIconButton,
+  type IconButtonProps as MuiIconButtonProps
+} from '@mui/material';
+import React from 'react';
 
-export function IconButton(props: IconButtonProps): JSX.Element {
-  return <MuiIconButton {...props} />;
-}
+export type IconButtonProps = MuiIconButtonProps;
+
+export const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => (
+  <MuiIconButton ref={ref} {...props} />
+));
+
+IconButton.displayName = 'IconButton';
+
+export default IconButton;
