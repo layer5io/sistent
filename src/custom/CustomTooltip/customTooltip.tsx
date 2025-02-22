5936d9b5456ca8a12d690fb37b53307fa3544cf2
mrmerlin320 <mrmerlin320@gmail.com>
2025-02-22 18:58:52 +0000

    fix: update custom tooltip styles
    Signed-off-by: mrmerlin320 <mrmerlin320@gmail.com>

--- src/custom/CustomTooltip/customTooltip.tsx
+++ src/custom/CustomTooltip/customTooltip.tsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { Tooltip, TooltipProps } from '../../base';
 import { WHITE } from '../../theme';
@@ -11,6 +12,7 @@ type CustomTooltipProps = {
   fontWeight?: number;
   variant?: 'standard' | 'small';
   bgColor?: string;
+  componentsProps?: TooltipProps['componentsProps'];
 } & Omit<TooltipProps, 'title' | 'onClick'>;
 
 function CustomTooltip({
@@ -22,34 +24,39 @@ function CustomTooltip({
   fontWeight = 400,
   variant = 'standard',
   bgColor = '#141414',
+  componentsProps = {},
   ...props
 }: CustomTooltipProps): JSX.Element {
   return (
     <Tooltip
-      componentsProps={{
-        tooltip: {
-          sx: {
-            background: bgColor,
-            color: WHITE,
-            fontSize: fontSize || (variant === 'standard' ? '1rem' : '0.75rem'),
-            fontWeight: { fontWeight },
-            borderRadius: '0.5rem',
-            padding: variant === 'standard' ? '0.9rem' : '0.5rem 0.75rem',
-            boxShadow: 'rgba(0, 0, 0, 0.6) 0px 4px 10px, rgba(0, 0, 0, 0.5) 0px 2px 4px'
+      componentsProps={_.merge(
+        {
+          tooltip: {
+            sx: {
+              background: bgColor,
+              color: WHITE,
+              maxWidth: '600px',
+              fontSize: fontSize || (variant === 'standard' ? '1rem' : '0.75rem'),
+              fontWeight: { fontWeight },
+              borderRadius: '0.5rem',
+              padding: variant === 'standard' ? '0.9rem' : '0.5rem 0.75rem',
+              boxShadow: 'rgba(0, 0, 0, 0.6) 0px 4px 10px, rgba(0, 0, 0, 0.5) 0px 2px 4px'
+            }
+          },
+          popper: {
+            sx: {
+              zIndex: 9999999999,
+              opacity: '1'
+            }
+          },
+          arrow: {
+            sx: {
+              color: bgColor
+            }
           }
         },
-        popper: {
-          sx: {
-            zIndex: 9999999999,
-            opacity: '1'
-          }
-        },
-        arrow: {
-          sx: {
-            color: bgColor
-          }
-        }
-      }}
+        componentsProps
+      )}
       title={typeof title === 'string' ? <RenderMarkdownTooltip content={title} /> : title}
       placement={placement}
       arrow
