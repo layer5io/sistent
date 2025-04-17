d6321c9fe24abb7acfade316a304c4c815dcb288
amitamrutiya <amitamrutiya2210@gmail.com>
2025-04-17 22:24:53 +0530

    feat: update icon
    Signed-off-by: amitamrutiya <amitamrutiya2210@gmail.com>

--- src/icons/Edit/EditIcon.tsx
+++ src/icons/Edit/EditIcon.tsx
@@ -7,13 +7,7 @@ export const EditIcon = ({
   ...props
 }: IconProps): JSX.Element => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 -960 960 960"
-      width={width}
-      height={height}
-      {...props}
-    >
+    <svg viewBox="0 0 24 24" width={height} height={width} {...props}>
       <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83 3.75 3.75z"></path>
     </svg>
   );
