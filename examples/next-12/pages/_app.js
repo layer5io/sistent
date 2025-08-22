0901252253f56bad3932e619bd4aab6a3b8df861
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-08-23 00:49:03 +0530

    restore redux
    Signed-off-by: saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>

--- examples/next-12/pages/_app.js
+++ examples/next-12/pages/_app.js
@@ -1,11 +1,13 @@
 import { AppThemeContext } from './../lib/context/AppThemeContext';
+import { Provider } from 'react-redux';
+import store from './../lib/redux/store';
 
 export default function App({ Component, pageProps }) {
   return (
-
+    <Provider store={store}>
       <AppThemeContext>
         <Component {...pageProps} />
       </AppThemeContext>
-
+    </Provider>
   );
 }
