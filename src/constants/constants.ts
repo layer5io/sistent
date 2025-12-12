1be222cb54b5c310b699eeb7924070e43add89a8
KSNauman <ksnauman639@gmail.com>
2025-12-12 12:58:53 +0000

    fix(icons): set Sistent icons to be filled by default using currentColor
    Signed-off-by: KSNauman <ksnauman639@gmail.com>

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_WIDTH = '24';
 export const DEFAULT_HEIGHT = '24';
 export const DEFAULT_FILL = '#000';
-export const DEFAULT_FILL_NONE = 'none';
+export const DEFAULT_FILL_NONE = 'currentColor';
 export const KEPPEL_GREEN_FILL = '#00B39F';
 export const CARIBBEAN_GREEN_FILL = '#00D3A9';
 export const DEFAULT_STROKE = '#000';
