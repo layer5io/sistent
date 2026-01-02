254ac2f8e4e6d48cd18a007adfe1b828d46e9749
Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>
2026-01-02 15:58:48 +0530

    fix
    Signed-off-by: Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>

--- src/icons/CrossCircle/index.tsx
+++ src/icons/CrossCircle/index.tsx
@@ -1,10 +1,10 @@
-import { DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../constants/constants';
+import { DEFAULT_FILL_NONE, DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../constants/constants';
 import { IconProps } from '../types';
 
 function CrossCircleIcon({
   width = DEFAULT_WIDTH,
   height = DEFAULT_HEIGHT,
-  fill = 'currentColor',
+  fill = DEFAULT_FILL_NONE,
   ...props
 }: IconProps): JSX.Element {
   return (
@@ -17,17 +17,9 @@ function CrossCircleIcon({
       style={{ ...props.style }}
       {...props}
     >
-      <g clipPath="url(#a)">
-        <path
-          d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2Zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59Z"
-          fillOpacity=".54"
-        />
-      </g>
-      <defs>
-        <clipPath id="a">
-          <path d="M0 0h24v24H0z" fill={props.stroke || '#FFFFF'} />
-        </clipPath>
-      </defs>
+     <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2m5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12z" 
+      fill={fill}
+       />
     </svg>
   );
 }
