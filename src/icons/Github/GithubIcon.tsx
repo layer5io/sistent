4274a916461e414c35739d7a11615132d8091e47
Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>
2026-01-06 20:44:51 +0530

    hanged the viewBox attribute improve visual appearance and alignment.
    Signed-off-by: Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>

--- src/icons/Github/GithubIcon.tsx
+++ src/icons/Github/GithubIcon.tsx
@@ -17,7 +17,7 @@ export default function GithubIcon({
       width={width}
       className={className}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 400 400"
+      viewBox="60 60 280 280"
     >
       <path
         fill={fill}
