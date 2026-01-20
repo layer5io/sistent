7beeb14e1cb160bee14dd97329f7b32b8f418118
Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>
2026-01-20 20:50:22 +0530

    addressed co-pilot review 1
    Signed-off-by: Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>

--- src/base/Hidden/Hidden.tsx
+++ src/base/Hidden/Hidden.tsx
@@ -1,12 +1,7 @@
 import { Hidden as MuiHidden, HiddenProps as MuiHiddenProps } from '@mui/material';
-import React from 'react';
 
-export const Hidden = React.forwardRef<HTMLDivElement, MuiHiddenProps>((props, ref) => {
-  return (
-    <div ref={ref}>
-      <MuiHidden {...props} />
-    </div>
-  );
-});
+export const Hidden = (props: MuiHiddenProps) => {
+  return <MuiHidden {...props} />;
+};
 
 export default Hidden;
