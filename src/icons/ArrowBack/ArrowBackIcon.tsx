04f683edc508738238d4de8a42661a2159f27a93
kishore08-07 <kishorebsm8@gmail.com>
2026-01-30 19:33:45 +0530

    added arrowback icon
    Signed-off-by: kishore08-07 <kishorebsm8@gmail.com>

--- src/icons/ArrowBack/ArrowBackIcon.tsx
+++ src/icons/ArrowBack/ArrowBackIcon.tsx
@@ -13,7 +13,6 @@ export const ArrowBackIcon = ({
       height={height}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      data-testid="arrow-back-icon-svg"
       {...props}
     >
       <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z" fill={fill} />
