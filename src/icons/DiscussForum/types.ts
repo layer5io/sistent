01265a2b0593b5b66baa38201cc2aedd43bf775f
Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>
2026-02-05 22:55:17 +0530

    Discuss forum icon
    Signed-off-by: Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>

---
+++ src/icons/DiscussForum/types.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface DiscussForumIconProps {
+  width?: number | string;
+  height?: number | string;
+  fill?: string;
+  primaryColor?: string;
+  secondaryColor?: string;
+  tertiaryColor?: string;
+  quaternaryColor?: string;
+  quinaryColor?: string;
+  senaryColor?: string;
+  className?: string;
+  style?: React.CSSProperties;
+}
