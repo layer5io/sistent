cae7a14254667336611d93c6f8435e23c6d35ea4
Bhumika Garg <bhumikatech2024@gmail.com>
2026-02-13 21:58:33 +0530

    Changes done in AccessTimeFilled
    Signed-off-by: Bhumika Garg <bhumikatech2024@gmail.com>

--- src/icons/AccessTimeFilled/AccessTimeFilledIcon.tsx
+++ src/icons/AccessTimeFilled/AccessTimeFilledIcon.tsx
@@ -1,10 +1,10 @@
-import { DEFAULT_FILL, DEFAULT_FILL_NONE, DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../constants/constants';
+import { DEFAULT_FILL_NONE, DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../constants/constants';
 import { IconProps } from '../types';
 
 export const AccessTimeFilledIcon = ({
   width = DEFAULT_WIDTH,
   height = DEFAULT_HEIGHT,
-  fill = DEFAULT_FILL,
+  fill = DEFAULT_FILL_NONE,
   ...props
 }: IconProps): JSX.Element => {
   return (
