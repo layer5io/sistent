918510ada73c5cf9650d22f698d3ce1c5c7c757b
Aman Karn <amankarn2007@gmail.com>
2026-04-19 14:28:19 +0530

    feat: add ArrowDownward icon
    Signed-off-by: Aman Karn <amankarn2007@gmail.com>

---
+++ src/icons/ArrowDownward/ArrowDownwardIcon.tsx
@@ -0,0 +1,23 @@
+import { DEFAULT_FILL_NONE, DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../constants/constants';
+import { IconProps } from '../types';
+
+export const ArrowDownwardIcon = ({
+  width = DEFAULT_WIDTH,
+  height = DEFAULT_HEIGHT,
+  fill = DEFAULT_FILL_NONE,
+  ...props
+}: IconProps): JSX.Element => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" fill={fill} />
+    </svg>
+  );
+};
+
+export default ArrowDownwardIcon;
