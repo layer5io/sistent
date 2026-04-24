770bb6cc15c60b18e77b8b61c4d6544c19133ed2
ITACHI161105 <utkarsha137@gmail.com>
2026-04-24 19:54:00 +0530

    fix: refactor ChevronRight icon structure as per review
    Signed-off-by: ITACHI161105 <utkarsha137@gmail.com>

--- src/icons/ChevronRight/ChevronRight.tsx
+++ src/icons/ChevronRight/ChevronRight.tsx
@@ -1,5 +1,5 @@
-import { DEFAULT_HEIGHT, DEFAULT_WIDTH,DEFAULT_FILL_NONE } from '../../constants/constants';
-import { ONYX_BLACK } from '../../theme';
+import { DEFAULT_HEIGHT, DEFAULT_WIDTH,DEFAULT_FILL_NONE } from 
+'../../constants/constants';
 import { IconProps } from '../types';
 
 export const ChevronRight = ({
@@ -17,7 +17,8 @@ export const ChevronRight = ({
       {...props}
     >
       <path
-        d="M8 16.997a1 1 0 01-.707-1.707l5-5a.999.999 0 000-1.414l-5-5a.999.999 0 111.414-1.414l5 5a3 3 0 010 4.243l-5 5A.997.997 0 018 16.997z"
+        d="M8 16.997a1 1 0 01-.707-1.707l5-5a.999.999 0 000-1.414l-5-5a.999.999 
+        0 111.414-1.414l5 5a3 3 0 010 4.243l-5 5A.997.997 0 018 16.997z"
         fill={fill}
       />
     </svg>
