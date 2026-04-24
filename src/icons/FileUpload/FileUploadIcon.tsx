03a10dff3e45136253cc10433fd638e2dc829a4e
shivam tyagi <tyagishivam.dev@gmail.com>
2026-04-24 20:45:17 +0530

    Add fill prop to FileUploadIcon path
    Signed-off-by: shivam tyagi <tyagishivam.dev@gmail.com>

--- src/icons/FileUpload/FileUploadIcon.tsx
+++ src/icons/FileUpload/FileUploadIcon.tsx
@@ -15,7 +15,8 @@ export const FileUploadIcon = ({
       viewBox="0 0 24 24"
       {...props}
     >
-      <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z"
+      <path
+        d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z"
         fill={fill}
       />
     </svg>
