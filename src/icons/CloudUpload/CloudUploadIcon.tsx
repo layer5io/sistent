9ce9c71e9559b4967daa69cc3b6202c3c9d8388d
Rishi Raj <rishiraj438gt@gmail.com>
2026-05-03 00:44:42 +0530

    fix(icon): remove duplicate svg tag
    Signed-off-by: Rishi Raj <rishiraj438gt@gmail.com>

--- src/icons/CloudUpload/CloudUploadIcon.tsx
+++ src/icons/CloudUpload/CloudUploadIcon.tsx
@@ -10,7 +10,6 @@ export const CloudUploadIcon = ({
 }: IconProps): JSX.Element => {
   return (
     <svg
-    <svg
       width={width}
       height={height}
       xmlns="http://www.w3.org/2000/svg"
