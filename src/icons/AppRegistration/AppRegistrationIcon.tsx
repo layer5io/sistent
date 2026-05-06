e677c515ca4f0fa71e4b4a5805d40a05ae5e5ae8
aprv10 <1apoorvindia@gmail.com>
2026-05-06 15:12:37 +0530

    feat: add AppRegistrationIcon to sistent
    Signed-off-by: aprv10 <1apoorvindia@gmail.com>

---
+++ src/icons/AppRegistration/AppRegistrationIcon.tsx
@@ -0,0 +1,25 @@
+import { DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../constants/constants';
+import { IconProps } from '../types';
+
+export const AppRegistrationIcon = ({
+  width = DEFAULT_WIDTH,
+  height = DEFAULT_HEIGHT,
+  ...props
+}: IconProps): JSX.Element => {
+  return (
+    <svg
+      aria-hidden="true"
+      viewBox="0 0 24 24"
+      data-testid="AppRegistrationIcon"
+      fill={props.fill || 'currentColor'}
+      width={width}
+      height={height}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M10 4h4v4h-4zM4 16h4v4H4zm0-6h4v4H4zm0-6h4v4H4zm10 8.42V10h-4v4h2.42zm6.88-1.13-1.17-1.17c-.16-.16-.42-.16-.58 0l-.88.88L20 12.75l.88-.88c.16-.16.16-.42 0-.58M11 18.25V20h1.75l6.67-6.67-1.75-1.75zM16 4h4v4h-4z"></path>
+    </svg>
+  );
+};
+
+export default AppRegistrationIcon;
---
+++ src/icons/AppRegistration/index.ts
@@ -0,0 +1,1 @@
+export { AppRegistrationIcon } from './AppRegistrationIcon';
