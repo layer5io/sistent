1ae55efe88afa44e02198ea3fff276f91404faa1
kanishksingh23 <kanishksingh763@gmail.com>
2026-06-01 15:52:48 +0000

    fix(icons): pass fill prop to path with default green color
    Signed-off-by: kanishksingh23 <kanishksingh763@gmail.com>

--- src/icons/FilterNone/FilterNoneIcon.tsx
+++ src/icons/FilterNone/FilterNoneIcon.tsx
@@ -1,9 +1,10 @@
-import { DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../constants/constants';
+import { DEFAULT_HEIGHT, DEFAULT_WIDTH, KEPPEL_GREEN_FILL } from '../../constants/constants';
 import { IconProps } from '../types';
 
 export const FilterNoneIcon = ({
   width = DEFAULT_WIDTH,
   height = DEFAULT_HEIGHT,
+  fill = KEPPEL_GREEN_FILL,
   ...props
 }: IconProps): JSX.Element => {
   return (
@@ -14,9 +15,13 @@ export const FilterNoneIcon = ({
       height={height}
       {...props}
     >
-      <path d="M3 5H1v16c0 1.1.9 2 2 2h16v-2H3V5zm18-4H7c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V3c0-1.1-.9-2-2-2zm0 16H7V3h14v14z" />
+      <path
+        d="M3 5H1v16c0 1.1.9 2 2 2h16v-2H3V5zm18-4H7c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V3c0-1.1-.9-2-2-2zm0 16H7V3h14v14z"
+        fill={fill}
+      />
     </svg>
   );
 };
 
 export default FilterNoneIcon;
+
