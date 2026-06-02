aca8b0659e500e296cfa8638449ce8c08d08616f
kanishksingh23 <kanishksingh763@gmail.com>
2026-06-02 04:51:25 +0000

    Fix import: use DEFAULT_FILL_NONE instead of KEPPEL_GREEN_FILL
    Signed-off-by: kanishksingh23 <kanishksingh763@gmail.com>

--- src/icons/FilterAll/FilterAllIcon.tsx
+++ src/icons/FilterAll/FilterAllIcon.tsx
@@ -1,4 +1,4 @@
-import { DEFAULT_HEIGHT, DEFAULT_WIDTH, KEPPEL_GREEN_FILL } from '../../constants/constants';
+import { DEFAULT_HEIGHT, DEFAULT_WIDTH, DEFAULT_FILL_NONE } from '../../constants/constants';
 import { IconProps } from '../types';
 
 export const FilterAllIcon = ({
