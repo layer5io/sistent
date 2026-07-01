45925f7a91ec785e53ba5f82d6de850e23174731
Parth Gartan <parthgartan26feb@gmail.com>
2026-07-01 05:27:38 +0000

    Apply {...props} to the <svg> tag.
    Signed-off-by: Parth Gartan <parthgartan26feb@gmail.com>

--- src/icons/Download/Download.tsx
+++ src/icons/Download/Download.tsx
@@ -6,14 +6,14 @@ export const DownloadIcon: FC<IconProps> = ({
   width = DEFAULT_WIDTH,
   height = DEFAULT_HEIGHT,
   fill = DEFAULT_FILL_NONE,
-  style = {}
+  ...props
 }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     height={height}
     viewBox="0 -960 960 960"
     width={width}
-    style={style}
+    {...props}
   >
     <path
       d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"
