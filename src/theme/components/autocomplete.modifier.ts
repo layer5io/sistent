1887da3ef68970e8f6e8f472ce3cf028ff3eb047
NSTKrishna <krishnagehlot936@gmail.com>
2026-07-01 18:55:56 +0530

    Circular dimensions for Autocomplete clear and popup indicators
    Signed-off-by: NSTKrishna <krishnagehlot936@gmail.com>

---
+++ src/theme/components/autocomplete.modifier.ts
@@ -0,0 +1,18 @@
+import { Theme, Components } from '@mui/material';
+
+export const MuiAutocomplete: Components<Theme>['MuiAutocomplete'] = {
+  styleOverrides: {
+    clearIndicator: {
+      boxShadow: 'none',
+      width: 28,
+      height: 28,
+      borderRadius: '50%'
+    },
+    popupIndicator: {
+      boxShadow: 'none',
+      width: 28,
+      height: 28,
+      borderRadius: '50%'
+    }
+  }
+};
