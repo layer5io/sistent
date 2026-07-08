78a26e84d6bd95b0753adfeee94c0cc374efeb16
Rishi Raj <rishiraj438gt@gmail.com>
2026-07-08 22:01:45 +0530

    fix-build
    Signed-off-by: Rishi Raj <rishiraj438gt@gmail.com>

--- src/base/ListItem/ListItem.tsx
+++ src/base/ListItem/ListItem.tsx
@@ -11,15 +11,17 @@ const ListItem = React.forwardRef<HTMLLIElement, ListItemProps>((props, ref) => 
   const { permissionKey, disabled, ...rest } = props;
 
   // When disabled AND permissionKey is provided, show the shield overlay
+  // Note: MUI ListItem doesn't have a native `disabled` prop, so we only use
+  // it as a custom guard for the PermissionShield wrapper
   if (disabled && permissionKey) {
     return (
       <PermissionShield permissionKey={permissionKey} variant="inline">
-        <MuiListItem {...rest} ref={ref} disabled={true} />
+        <MuiListItem {...rest} ref={ref} />
       </PermissionShield>
     );
   }
 
-  return <MuiListItem {...rest} ref={ref} disabled={disabled} />;
+  return <MuiListItem {...rest} ref={ref} />;
 });
 
 ListItem.displayName = 'ListItem';
