f6c2b655dc8465fa3e4b85fabf0430d60b60aaae
Utkarsh Maurya <deepraj5484@gmail.com>
2026-08-05 01:28:05 +0530

    added explicit return type
    Signed-off-by: Utkarsh Maurya <deepraj5484@gmail.com>

--- src/base/ListItemText/ListItemText.tsx
+++ src/base/ListItemText/ListItemText.tsx
@@ -6,14 +6,14 @@ import type { ListItemTextOwnerState } from '@mui/material/ListItemText';
 
 function withNoWrapDefault<T extends object>(
   slotProp: T | ((ownerState: ListItemTextOwnerState) => T) | undefined
-) {
+):T | ((ownerState: ListItemTextOwnerState) => T) {
   if (typeof slotProp === 'function') {
     return (ownerState: ListItemTextOwnerState) => ({
       noWrap: true,
       ...slotProp(ownerState)
     });
   }
-  return { noWrap: true, ...slotProp };
+  return { noWrap: true, ...slotProp } as T;
 }
 
 export function ListItemText({
